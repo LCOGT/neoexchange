46e6ea7c61110a5fec6367363e01bb3b39414555
Tim Lister <tlister@lcogt.net>
2015-03-04 02:40:12 -0800

    Update preambles.

--- ingest/ast_subs.py
+++ ingest/ast_subs.py
@@ -1,3 +1,20 @@
+'''
+NEO exchange: NEO observing portal for Las Cumbres Observatory Global Telescope Network
+Copyright (C) 2014-2015 LCOGT
+
+ast_subs.py -- Asteroid desigination related routines.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+'''
+
 import re
 
 class PackedError(Exception):
