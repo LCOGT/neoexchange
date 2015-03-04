de015a7a86962cb740b63324c248ca2e838046e5
Tim Lister <tlister@lcogt.net>
2015-03-04 02:30:10 -0800

    Add new set of asteroid desigination related routines.
    ingest/ast_subs.py: Add normal_to_packed() method plus associated helper method and error/exception classes for converting normal desiginations to packed format.
    ingest/tests.py: Add tests for normal_to_packed() and int_to_mutant_hex_char().

--- ingest/tests.py
+++ ingest/tests.py
@@ -13,3 +13,214 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 '''
 
+from nose.tools import eq_, assert_equal, assert_almost_equal, raises, nottest
+
+#Import module to test
+from ast_subs import *
+
+class TestIntToMutantHexChar(object):
+    '''Unit tests for the int_to_mutant_hex_char() method'''
+    
+# EricS says to run without the exception handler in place first to check it works as expected
+# This is the simple test version where we only care if we get an exception (any exception)
+# and are not bothered what the specific message/failure is
+# '@raises' is a "decorator"
+
+#    @raises(MutantError)
+#    def test_bad_mutantcode_length(self):
+#        int_to_mutant_hex_char(-1)
+
+    def test_bad_mutant_too_small(self):
+        try:
+            int_to_mutant_hex_char(-1)
+            assert False
+        except MutantError as e:
+            expected_msg = ("Number out of range 0...61")
+            assert_equal(e.__str__(), expected_msg)
+
+    def test_bad_mutant_too_large(self):
+        try:
+            int_to_mutant_hex_char(62)
+            assert False
+        except MutantError as e:
+            expected_msg = ("Number out of range 0...61")
+            assert_equal(e.__str__(), expected_msg)
+
+    def test_bad_mutant_not_number1(self):
+        try:
+            int_to_mutant_hex_char('9')
+            assert False
+        except MutantError as e:
+            expected_msg = ("Number out of range 0...61")
+            assert_equal(e.__str__(), expected_msg)
+
+    def test_bad_mutant_not_number2(self):
+        try:
+            int_to_mutant_hex_char('FOO')
+            assert False
+        except MutantError as e:
+            expected_msg = ("Number out of range 0...61")
+            assert_equal(e.__str__(), expected_msg)
+
+    def test_num_less_than_ten(self):
+        char = int_to_mutant_hex_char(8)
+        expected_char = '8'
+        assert_equal(char, expected_char)
+
+    def test_num_less_than_thirtysix_t1(self):
+        char = int_to_mutant_hex_char(10)
+        expected_char = 'A'
+        assert_equal(char, expected_char)
+
+    def test_num_less_than_thirtysix_t2(self):
+        char = int_to_mutant_hex_char(23)
+        expected_char = 'N'
+        assert_equal(char, expected_char)
+
+    def test_num_less_than_thirtysix_t3(self):
+        char = int_to_mutant_hex_char(34)
+        expected_char = 'Y'
+        assert_equal(char, expected_char)
+
+    def test_num_less_than_thirtysix_t4(self):
+        char = int_to_mutant_hex_char(35)
+        expected_char = 'Z'
+        assert_equal(char, expected_char)
+
+    def test_num_greater_than_thirtysix_t1(self):
+        char = int_to_mutant_hex_char(36)
+        expected_char = 'a'
+        assert_equal(char, expected_char)
+
+    def test_num_greater_than_thirtysix_t2(self):
+        char = int_to_mutant_hex_char(49)
+        expected_char = 'n'
+        assert_equal(char, expected_char)
+
+    def test_num_greater_than_thirtysix_t3(self):
+        char = int_to_mutant_hex_char(60)
+        expected_char = 'y'
+        assert_equal(char, expected_char)
+
+    def test_num_greater_than_thirtysix_t4(self):
+        char = int_to_mutant_hex_char(61)
+        expected_char = 'z'
+        assert_equal(char, expected_char)
+
+
+class TestNormalToPacked(object):
+    '''Unit tests for normal_to_packed() method'''
+    
+    def test_number_t1(self):
+        expected_desig = '00719       '
+        packed_desig, ret_code = normal_to_packed('719')
+        assert_equal(packed_desig, expected_desig)
+        assert_equal(ret_code, 0)
+    
+    def test_number_t2(self):
+        expected_desig = 'B7317       '
+        packed_desig, ret_code = normal_to_packed('117317')
+        assert_equal(packed_desig, expected_desig)
+        assert_equal(ret_code, 0)
+    
+    def test_number_t3(self):
+        expected_desig = 'Z7317       '
+        packed_desig, ret_code = normal_to_packed('357317')
+        assert_equal(packed_desig, expected_desig)
+        assert_equal(ret_code, 0)
+    
+    def test_number_t4(self):
+        expected_desig = 'a7317       '
+        packed_desig, ret_code = normal_to_packed('367317')
+        assert_equal(packed_desig, expected_desig)
+        assert_equal(ret_code, 0)
+    
+    def test_number_t5(self):
+        expected_desig = 'g1234       '
+        packed_desig, ret_code = normal_to_packed('421234')
+        assert_equal(packed_desig, expected_desig)
+        assert_equal(ret_code, 0)
+    
+    def test_comet_t1(self):
+        expected_desig = '    CK13A010'
+        packed_desig, ret_code = normal_to_packed('C/2013 A1')
+        assert_equal(packed_desig, expected_desig)
+        assert_equal(ret_code, 0)
+    
+    def test_comet_t2(self):
+        expected_desig = '    PK01T000'
+        packed_desig, ret_code = normal_to_packed('P/2001 T')
+        assert_equal(packed_desig, expected_desig)
+        assert_equal(ret_code, 0)
+    
+    def test_comet_t3(self):
+        expected_desig = '0004P       '
+        packed_desig, ret_code = normal_to_packed('P/4')
+        assert_equal(packed_desig, expected_desig)
+        assert_equal(ret_code, 0)
+    
+    def test_comet_t4(self):
+        expected_desig = '0314P       '
+        packed_desig, ret_code = normal_to_packed('P/314')
+        assert_equal(packed_desig, expected_desig)
+        assert_equal(ret_code, 0)
+    
+    def test_comet_t5(self):
+        expected_desig = '    CJ83Z150'
+        packed_desig, ret_code = normal_to_packed('C/1983 Z15')
+        assert_equal(packed_desig, expected_desig)
+        assert_equal(ret_code, 0)
+    
+    def test_provdesig_t1(self):
+        expected_desig = '     K15D00D'
+        packed_desig, ret_code = normal_to_packed('2015   DD')
+        assert_equal(packed_desig, expected_desig)
+        assert_equal(ret_code, 0)
+    
+    def test_provdesig_t2(self):
+        expected_desig = '     J99Z01A'
+        packed_desig, ret_code = normal_to_packed('1999 ZA1')
+        assert_equal(packed_desig, expected_desig)
+        assert_equal(ret_code, 0)
+    
+    def test_provdesig_t3(self):
+        expected_desig = '     J99Z11A'
+        packed_desig, ret_code = normal_to_packed('1999 ZA11')
+        assert_equal(packed_desig, expected_desig)
+        assert_equal(ret_code, 0)
+    
+    def test_provdesig_t4(self):
+        expected_desig = '     K00LA0W'
+        packed_desig, ret_code = normal_to_packed('2000 LW100')
+        assert_equal(packed_desig, expected_desig)
+        assert_equal(ret_code, 0)
+
+    def test_provdesig_t5(self):
+        expected_desig = '     K10LZ9W'
+        packed_desig, ret_code = normal_to_packed('2010 LW359')
+        assert_equal(packed_desig, expected_desig)
+        assert_equal(ret_code, 0)
+    
+    def test_provdesig_t6(self):
+        expected_desig = '     K00La0W'
+        packed_desig, ret_code = normal_to_packed('2000 LW360')
+        assert_equal(packed_desig, expected_desig)
+        assert_equal(ret_code, 0)
+    
+    def test_provdesig_t7(self):
+        expected_desig = '     K00Az9Z'
+        packed_desig, ret_code = normal_to_packed('2000 AZ619')
+        assert_equal(packed_desig, expected_desig)
+        assert_equal(ret_code, 0)
+    
+    def test_baddesig_t1(self):
+        expected_desig = '            '
+        packed_desig, ret_code = normal_to_packed('FOO BAR')
+        assert_equal(packed_desig, expected_desig)
+        assert_equal(ret_code, -1)
+    
+    def test_baddesig_t1(self):
+        expected_desig = '            '
+        packed_desig, ret_code = normal_to_packed('12345 A')
+        assert_equal(packed_desig, expected_desig)
+        assert_equal(ret_code, -1)
