9010f0da9e74f72327f53966576f0d0801895aae
Tim Lister <tlister@lcogt.net>
2015-09-25 17:10:33 -0700

    Modify NEOCP updating management command to handle extra information from new NEOCP page parser.

--- neoexchange/core/management/commands/update_neocp_data.py
+++ neoexchange/core/management/commands/update_neocp_data.py
@@ -1,4 +1,4 @@
-from astrometrics.sources_subs import fetch_NEOCP, parse_NEOCP
+from astrometrics.sources_subs import fetch_NEOCP, parse_NEOCP_extra_params
 from core.views import update_NEOCP_orbit
 
 from django.core.management.base import BaseCommand, CommandError
@@ -11,11 +11,13 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         self.stdout.write("==== Fetching NEOCP targets %s ====" % (datetime.now().strftime('%Y-%m-%d %H:%M')))
         neocp_page = fetch_NEOCP()
-        obj_ids = parse_NEOCP(neocp_page)
+        obj_ids = parse_NEOCP_extra_params(neocp_page)
         self.stdout.write("==== Found %s NEOCP targets ====" % len(obj_ids))
         for obj_id in obj_ids:
-            self.stdout.write("Reading NEOCP target %s" % obj_id)
-            resp = update_NEOCP_orbit(str(obj_id))
+            obj_name = obj_id[0]
+            obj_extra_params = obj_id[1]
+            self.stdout.write("Reading NEOCP target %s" % obj_name)
+            resp = update_NEOCP_orbit(str(obj_name), obj_extra_params)
             if resp:
                 self.stdout.write(resp)
             
