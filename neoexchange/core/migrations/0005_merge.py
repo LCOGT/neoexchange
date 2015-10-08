aee2f582332b7ed74af12a52218132847984f5f0
Tim Lister <tlister@lcogt.net>
2015-10-07 17:27:48 -0700

    Add migration to resolve dependency order caused by conflicting numbered migrations from the 2 recent branches

---
+++ neoexchange/core/migrations/0005_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0004_auto_20150925_2256'),
+        ('core', '0004_auto_20150930_0008'),
+    ]
+
+    operations = [
+    ]
