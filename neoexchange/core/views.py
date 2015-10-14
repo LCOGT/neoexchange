ed10b50754419361856960035d7e960d518a1eae
Tim Lister <tlister@lcogt.net>
2015-10-14 12:18:05 -0700

    Add new code to resolve problem of looking objects with no provisional name.

--- neoexchange/core/views.py
+++ neoexchange/core/views.py
@@ -316,7 +316,15 @@ def check_for_block(form_data, params, new_body):
         site_list = { 'V37' : 'ELP' , 'K92' : 'CPT', 'Q63' : 'COJ', 'W85' : 'LSC', 'W86' : 'LSC', 'F65' : 'OGG', 'E10' : 'COJ' }
 
         try:
-            block_id = Block.objects.get(body=Body.objects.get(provisional_name=new_body.provisional_name),
+            body_id = Body.objects.get(provisional_name=new_body.provisional_name)
+        except Body.MultipleObjectsReturned:
+            body_id = Body.objects.get(name=new_body.name)
+        except Body.DoesNotExist:
+            logger.warn("Body does not exist")
+            return 3
+
+        try:
+            block_id = Block.objects.get(body=body_id,
                                          groupid__contains=form_data['group_id'],
                                          proposal=Proposal.objects.get(code=form_data['proposal_code']),
                                          site=site_list[params['site_code']])
