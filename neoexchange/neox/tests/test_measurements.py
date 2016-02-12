1e7cb077a88d54e31799950795c3737d34ae5756
Tim Lister <tlister@lcogt.net>
2016-02-11 21:33:46 -0800

    Add new FT to check formatting of satellite entries in MPC output

--- neoexchange/neox/tests/test_measurements.py
+++ neoexchange/neox/tests/test_measurements.py
@@ -51,6 +51,40 @@ class MeasurementsPageTests(FunctionalTest):
                          }
         self.test_measure1 = SourceMeasurement.objects.create(pk=1, **measure_params)
 
+    def insert_satellite_test_measurements(self):
+        sat_frame_params = { 'sitecode' : 'C51',
+                         'filter' : 'R',
+                         'midpoint' : '2016-02-10 06:26:57',
+                         'frametype' : Frame.SATELLITE_FRAMETYPE,
+                         'extrainfo' : '     N009ags  s2016 02 10.26872 1 - 3333.4505 - 5792.1311 - 1586.1945   NEOCPC51'
+                        }
+        self.sat_test_frame = Frame.objects.create(pk=1, **sat_frame_params)
+
+        measure_params = { 'body' : self.body,
+                           'frame' : self.sat_test_frame,
+                           'obs_ra' : 229.21470833333336,
+                           'obs_dec' : -10.464194444444443,
+                         }
+        self.test_measure1 = SourceMeasurement.objects.create(pk=1, **measure_params)
+        frame_params = { 'sitecode' : 'W86',
+                         'instrument' : 'fl03',
+                         'filter' : 'R',
+                         'filename' : 'lsc1m009-fl03-20160210-0243-e10.fits',
+                         'exptime' : 95.0,
+                         'midpoint' : '2016-02-11 07:41:44',
+                         'block' : self.test_block
+                        }
+        self.test_frame = Frame.objects.create(pk=2, **frame_params)
+
+        measure_params = { 'body' : self.body,
+                           'frame' : self.test_frame,
+                           'obs_ra' : 229.66829166666668,
+                           'obs_dec' : -10.953888888888889,
+                           'obs_mag' : 20.1,
+                           'err_obs_mag' : 0.1
+                         }
+        self.test_measure1 = SourceMeasurement.objects.create(pk=2, **measure_params)
+
     def test_measurements_page(self):
 
         # Setup
@@ -132,7 +166,7 @@ class MeasurementsPageTests(FunctionalTest):
         self.browser.get(target_url)
 
         # He sees a link that says it will export the measurements
-        # available for this object in MPC 80.
+        # available for this object in MPC 80 char format.
         link = self.browser.find_element_by_partial_link_text('Show Measurements')
         target_url = "%s/target/%d/measurements/" % (self.live_server_url, 1)
         self.assertEqual(link.get_attribute('href'), target_url)
@@ -158,6 +192,54 @@ class MeasurementsPageTests(FunctionalTest):
         # discovery observations from WISE (obs. code C51) and from
         # the LCOGT follow-up network.
         testlines = [u'     N999r0q  C2015 04 20.75000002 48 24.00 -30 03 00.0          21.1 w      K91',
+                    ]
+        pre_block = self.browser.find_element_by_tag_name('pre')
+        rows = pre_block.text.splitlines()
+        for test_text in testlines:
+            self.assertIn(test_text, [row.replace('\n', ' ') for row in rows])
+
+        # Satisfied that the planet is safe from this asteroid, he
+        # leaves.
+
+    def test_satellite_measurements_mpc_format(self):
+
+        # Setup
+        self.insert_satellite_test_measurements()
+
+        # A user, James, is interested in seeing what existing measurements
+        # exist for a NEOCP candidate that he has heard about
+        target_url = self.live_server_url + reverse('target',kwargs={'pk':1})
+        self.browser.get(target_url)
+
+        # He sees a link that says it will show the source measurements
+        # available for this object.
+        link = self.browser.find_element_by_partial_link_text('Show Measurements')
+        target_url = "%s/target/%d/measurements/" % (self.live_server_url, 1)
+        self.assertEqual(link.get_attribute('href'), target_url)
+
+        # He clicks on the link and sees that he is taken to a page with details
+        # on the source measurements for this object
+        link.click()
+
+        self.assertEqual(self.browser.current_url, target_url)
+        header_text = self.browser.find_element_by_class_name('headingleft').text
+        self.assertIn('Source Measurements for: ' + self.body.current_name(), header_text)
+
+        # He sees a link that says it will display the measurements in MPC format
+        mpc_link = self.browser.find_element_by_partial_link_text('View in MPC format')
+        mpc_target_url = "%s/target/%d/measurements/mpc/" % (self.live_server_url, 1)
+        self.assertEqual(mpc_link.get_attribute('href'), mpc_target_url)
+ 
+        # He clicks on the link and sees that he is taken to a page with the 
+        # source measurements for this object in MPC 80 char format
+        mpc_link.click()
+
+        # He sees that there is a table in which are the original
+        # discovery observations from WISE (obs. code C51) and from
+        # the LCOGT follow-up network.
+        testlines = [u'     N999r0q  S2016 02 10.26872 15 16 51.53 -10 27 51.1                LNEOCPC51',
+                     u'     N999r0q  s2016 02 10.26872 1 - 3333.4505 - 5792.1311 - 1586.1945   NEOCPC51',
+                     u'     N999r0q  C2016 02 11.32064915 18 40.39 -10 57 14.0          20.1 RtNEOCPW86',
                     ]
         pre_block = self.browser.find_element_by_tag_name('pre')
         rows = pre_block.text.splitlines()
