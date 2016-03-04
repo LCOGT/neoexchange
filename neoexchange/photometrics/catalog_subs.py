fde3137cb51c05a132eec2ed80249f6c58b73522
Sarah Greenstreet <sgreenstreet@lcogt.net>
2016-03-04 14:06:48 -0800

    Modified get_catalog_table to make sure getting all of the requested table.

--- neoexchange/photometrics/catalog_subs.py
+++ neoexchange/photometrics/catalog_subs.py
@@ -15,12 +15,25 @@ logger = logging.getLogger(__name__)
 def get_catalog_table(ra, dec, cat = "PPMXL", set_row_limit = 10000, rmag_limit = "<=15.0", set_width = "30m"):
     '''Pulls a catalog from Vizier'''
 
-    #query Vizier on a region of the sky with ra and dec coordinates a specified catalog
-    query_service = Vizier(row_limit=set_row_limit, column_filters={"r2mag":rmag_limit, "r1mag":rmag_limit})
-    result = query_service.query_region(coord.SkyCoord(ra, dec, unit=(u.deg, u.deg), frame='icrs'), width=set_width, catalog=[cat])
+    #query Vizier on a region of the sky with ra and dec coordinates of a specified catalog
+    while set_row_limit < 100000:
+        
+        query_service = Vizier(row_limit=set_row_limit, column_filters={"r2mag":rmag_limit, "r1mag":rmag_limit})
+        result = query_service.query_region(coord.SkyCoord(ra, dec, unit=(u.deg, u.deg), frame='icrs'), width=set_width, catalog=[cat])
 
-    #resulting catalog table
-    cat_table = result[0]
+        #resulting catalog table
+        cat_table = result[0]
+
+        #if didn't get all of the table, try again with a larger row limit
+        if len(cat_table) == set_row_limit:
+            set_row_limit += 10000
+            query_service = Vizier(row_limit=set_row_limit, column_filters={"r2mag":rmag_limit, "r1mag":rmag_limit})
+            result = query_service.query_region(coord.SkyCoord(ra, dec, unit=(u.deg, u.deg), frame='icrs'), width=set_width, catalog=[cat])
+
+            #resulting catalog table
+            cat_table = result[0]
+        else:
+            break
 
     return cat_table
 
