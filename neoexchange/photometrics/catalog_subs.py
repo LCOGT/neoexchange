26be9c85d97e0eafd23bb77830663f3d4b045979
Sarah Greenstreet <sgreenstreet@lcogt.net>
2016-03-09 13:32:06 -0800

    Generalized cross_match code a bit.

--- neoexchange/photometrics/catalog_subs.py
+++ neoexchange/photometrics/catalog_subs.py
@@ -14,14 +14,14 @@ from astropy.table import Table
 
 logger = logging.getLogger(__name__)
 
-def get_catalog_table(ra, dec, cat = "PPMXL", set_row_limit = 10000, rmag_limit = "<=15.0", set_width = "30m"):
+def get_catalog_table(ra, dec, cat_name = "PPMXL", set_row_limit = 10000, rmag_limit = "<=15.0", set_width = "30m"):
     '''Pulls a catalog from Vizier'''
 
     #query Vizier on a region of the sky with ra and dec coordinates of a specified catalog
     while set_row_limit < 100000:
         
         query_service = Vizier(row_limit=set_row_limit, column_filters={"r2mag":rmag_limit, "r1mag":rmag_limit})
-        result = query_service.query_region(coord.SkyCoord(ra, dec, unit=(u.deg, u.deg), frame='icrs'), width=set_width, catalog=[cat])
+        result = query_service.query_region(coord.SkyCoord(ra, dec, unit=(u.deg, u.deg), frame='icrs'), width=set_width, catalog=[cat_name])
 
         #resulting catalog table
         cat_table = result[0]
@@ -30,7 +30,7 @@ def get_catalog_table(ra, dec, cat = "PPMXL", set_row_limit = 10000, rmag_limit 
         if len(cat_table) == set_row_limit:
             set_row_limit += 10000
             query_service = Vizier(row_limit=set_row_limit, column_filters={"r2mag":rmag_limit, "r1mag":rmag_limit})
-            result = query_service.query_region(coord.SkyCoord(ra, dec, unit=(u.deg, u.deg), frame='icrs'), width=set_width, catalog=[cat])
+            result = query_service.query_region(coord.SkyCoord(ra, dec, unit=(u.deg, u.deg), frame='icrs'), width=set_width, catalog=[cat_name])
 
             #resulting catalog table
             cat_table = result[0]
@@ -57,26 +57,34 @@ def cross_match(cat_table_1, cat_table_2, cross_match_diff_threshold = 0.001):
         cat_table_1 = cat_table_2
         cat_table_2 = cat_table_temp
 
+    RA_cat_table_1 = cat_table_1['_RAJ2000']
+    Dec_cat_table_1 = cat_table_1['_DEJ2000']
+    try:
+        rmag_cat_table_1 = cat_table_1['rmag']
+    except:
+        rmag_cat_table_1 = cat_table_1['r2mag']
+
+    RA_cat_table_2 = cat_table_2['_RAJ2000']
+    Dec_cat_table_2 = cat_table_2['_DEJ2000']
+    try:
+        rmag_cat_table_2 = cat_table_2['rmag']
+    except:
+        rmag_cat_table_2 = cat_table_2['r2mag']
+
     y = 0
-    for value in cat_table_1['_DEJ2000']:
-        try:
-            rmag_cat_1_test = cat_table_1['rmag'][y]
-        except:
-            rmag_cat_1_test = cat_table_1['r2mag'][y]
+    for value in Dec_cat_table_1:
+        rmag_cat_1_test = rmag_cat_table_1[y]
         if rmag_cat_1_test > 0.0:
             ra_min_diff = ra_min_diff_threshold
             dec_min_diff = dec_min_diff_threshold
             z = 0
-            for source in cat_table_2['_DEJ2000']:
-                try:
-                    rmag_cat_2_test = cat_table_2['rmag'][z]
-                except:
-                    rmag_cat_2_test = cat_table_2['r2mag'][z]
+            for source in Dec_cat_table_2:
+                rmag_cat_2_test = rmag_cat_table_2[z]
                 if rmag_cat_2_test > 0.0:
                     if abs(source - value) < dec_min_diff:
                         dec_min_diff = abs(source - value)
-                        ra_cat_1_test = cat_table_1['_RAJ2000'][y]
-                        ra_cat_2_test = cat_table_2['_RAJ2000'][z]
+                        ra_cat_1_test = RA_cat_table_1[y]
+                        ra_cat_2_test = RA_cat_table_2[z]
                         if abs(ra_cat_1_test - ra_cat_2_test) < ra_min_diff:
                             ra_min_diff = abs(ra_cat_1_test - ra_cat_2_test)
                             dec_cat_1 = value
