e5093846ca931bc6e74f71656f272e00c9d6a37d
Tim Lister <tlister@lcogt.net>
2016-07-15 14:44:21 +0200

    Add copyright notice and fix import orders

--- neoexchange/core/frame_proc.py
+++ neoexchange/core/frame_proc.py
@@ -1,14 +1,29 @@
+'''
+NEO exchange: NEO observing portal for Las Cumbres Observatory Global Telescope Network
+Copyright (C) 2014-2016 LCOGT
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+'''
+
+import logging
+
+import requests
 from django.conf import settings
 from core.models import Frame
 from astrometrics.ephem_subs import call_compute_ephem, compute_ephem, \
     determine_darkness_times, determine_slot_length, determine_exp_time_count, \
-    MagRangeError,  LCOGT_site_codes
-import requests
-import logging
+    MagRangeError,  LCOGT_site_codes, LCOGT_domes_to_site_codes
 
 logger = logging.getLogger(__name__)
 
-from astrometrics.ephem_subs import LCOGT_domes_to_site_codes
 
 def fetch_from_archive(tracking_num, proposal_code):
     filters = []
