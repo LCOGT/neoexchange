5a600583e79bb70d13218d0d06a6dae49ba2279d
Tim Lister <tlister@lcogt.net>
2016-09-21 16:52:04 -0700

    Improvements to lightcurve extraction routine:
    Include minor ticks and save the figure.
    Resolve multiple source matches by looking for nearest source within a (user-specifiable) delta magnitude.

--- neoexchange/core/management/commands/lightcurve_extraction.py
+++ neoexchange/core/management/commands/lightcurve_extraction.py
@@ -1,9 +1,11 @@
 import os
 from sys import exit
 from datetime import datetime
+from math import degrees, radians
 
 from django.core.management.base import BaseCommand, CommandError
 from django.forms.models import model_to_dict
+import slalib as S
 import matplotlib.pyplot as plt
 from matplotlib.dates import HourLocator, DateFormatter
 
@@ -18,6 +20,7 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('blocknum', type=int, help='Block number to analyze')
         parser.add_argument('-bw', '--boxwidth', type=float, default=5.0, help='Boxwidth in arcsec to search')
+        parser.add_argument('-dm', '--deltamag', type=float, default=0.5, help='delta magnitude tolerance for multiple matches')
 
     def plot_timeseries(self, times, mags, mag_errs, colors='r', title=''):
         fig, ax = plt.subplots()
@@ -27,9 +30,11 @@ class Command(BaseCommand):
         ax.set_xlabel('Time')
         ax.set_ylabel('Magnitude')
         ax.set_title(title)
+        ax.minorticks_on()
         ax.xaxis.set_major_formatter(DateFormatter('%H:%M:%S'))
         ax.fmt_xdata = DateFormatter('%H:%M:%S')
         fig.autofmt_xdate()
+        plt.savefig("lightcurve.png")
         plt.show()
 
         return
@@ -48,7 +53,7 @@ class Command(BaseCommand):
 
         frames = Frame.objects.filter(block=block.id, zeropoint__isnull=False)
         self.stdout.write("Found %d frames for Block# %d with good ZPs" % (len(frames), block.id))
-        self.stdout.write("Searching within %.1f arcseconds" % (options['boxwidth']))
+        self.stdout.write("Searching within %.1f arcseconds and +/-%.1f delta magnitudes" % (options['boxwidth'], options['deltamag']))
 
         if len(frames) != 0:
             elements = model_to_dict(block.body)
@@ -58,14 +63,31 @@ class Command(BaseCommand):
             mag_errs = []
             for frame in frames:
                 emp_line = compute_ephem(frame.midpoint, elements, frame.sitecode)
-                (ra_string, dec_string) = radec2strings(emp_line[1], emp_line[2], ' ')
-                sources = search_box(frame, emp_line[1], emp_line[2], options['boxwidth'])
+                ra  = emp_line[1]
+                dec = emp_line[2]
+                mag_estimate = emp_line[3]
+                (ra_string, dec_string) = radec2strings(ra, dec, ' ')
+                sources = search_box(frame, ra, dec, options['boxwidth'])
                 self.stdout.write("%s %s %s %s (%d) %s" % (frame.midpoint, ra_string, dec_string, frame.sitecode, len(sources), frame.filename))
-                if len(sources) == 1:
+                if len(sources) != 0:
+                    if len(sources) == 1:
+                        best_source = sources[0]
+    #                    print "%.3f+/-%.3f" % (source.obs_mag, source.err_obs_mag)
+                    elif len(sources) > 1:
+                        min_sep = options['boxwidth'] * options['boxwidth']
+                        for source in sources:
+                            sep = S.sla_dsep(ra, dec, radians(source.obs_ra), radians(source.obs_dec))
+                            sep = degrees(sep) * 3600.0
+                            src_ra_string, src_dec_string = radec2strings(radians(source.obs_ra), radians(source.obs_dec))
+                            delta_mag = abs(mag_estimate - source.obs_mag)
+                            self.stdout.write("%s %s %s %s %.1f %.1f-%.1f %.1f" % ( ra_string, dec_string, src_ra_string, src_dec_string, sep, mag_estimate, source.obs_mag, delta_mag))
+                            if sep < min_sep and delta_mag <= options['deltamag']:
+                                min_sep = sep
+                                best_source = source
+
                     times.append(frame.midpoint)
-                    source = sources[0]
-                    mags.append(source.obs_mag)
-                    mag_errs.append(source.err_obs_mag)
-#                    print "%.3f+/-%.3f" % (source.obs_mag, source.err_obs_mag)
+                    mags.append(best_source.obs_mag)
+                    mag_errs.append(best_source.err_obs_mag)
+  
             plot_title = '%s from %s (%s) on %s' % (block.body.current_name(), block.site.upper(), frame.sitecode, block.when_observed.strftime("%Y-%m-%d"))
             self.plot_timeseries(times, mags, mag_errs, title=plot_title)
