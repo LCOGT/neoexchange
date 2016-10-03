681c4eafde4ec2cb0be985b95f84a241a9c64517
Tim Lister <tlister@lcogt.net>
2016-10-03 08:56:46 -0700

    Change default proposal for 2016B semester

--- neoexchange/core/management/commands/download_archive_data.py
+++ neoexchange/core/management/commands/download_archive_data.py
@@ -12,7 +12,7 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('--date', action="store", default=datetime.utcnow(), help='Date of the data to download')
-        parser.add_argument('--proposal', action="store", default="LCO2016A-021", help='Proposal code to query for data')
+        parser.add_argument('--proposal', action="store", default="LCO2016B-011", help='Proposal code to query for data')
 
 
     def handle(self, *args, **options):
