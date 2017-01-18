d3f1fc5769c895c4f2110f299f600b783c05e161
Edward Gomez <edward@gomez.me.uk>
2017-01-18 11:43:49 +0000

    Adding jitter and period to confirm

--- neoexchange/core/forms.py
+++ neoexchange/core/forms.py
@@ -75,8 +75,8 @@ class ScheduleForm(forms.Form):
 class ScheduleCadenceForm(forms.Form):
     proposal_code = forms.ChoiceField(required=True)
     site_code = forms.ChoiceField(required=True, choices=SITES)
-    start_time = forms.DateTimeField(input_formats=['%Y-%m-%d %H:%M:%S',], initial=datetime.today, required=True, error_messages={'required': _(u'UTC start date is required')})
-    end_time = forms.DateTimeField(input_formats=['%Y-%m-%d %H:%M:%S',], initial=datetime.today, required=True, error_messages={'required': _(u'UTC end date is required')})
+    start_time = forms.DateTimeField(input_formats=['%Y-%m-%d %H:%M:%S', '%Y-%m-%dT%H:%M:%S'], initial=datetime.today, required=True, error_messages={'required': _(u'UTC start date is required')})
+    end_time = forms.DateTimeField(input_formats=['%Y-%m-%d %H:%M:%S', '%Y-%m-%dT%H:%M:%S'], initial=datetime.today, required=True, error_messages={'required': _(u'UTC end date is required')})
     period = forms.FloatField(initial=2.0, required=True, widget=forms.TextInput(attrs={'size':'10'}), error_messages={'required': _(u'Period is required')})
     jitter = forms.FloatField(initial=0.25, required=True, widget=forms.TextInput(attrs={'size':'10'}), error_messages={'required': _(u'Jitter is required')})
 
