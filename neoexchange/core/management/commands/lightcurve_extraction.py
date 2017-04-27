813f717ee5fc32b3e838cff2df6e04ad926e98d2
Tim Lister <tlister@lcogt.net>
2017-04-27 00:32:53 +0000

    Used a fixed JD offset derived from first frame. Add header to output file and add this to the .gitignore

--- neoexchange/core/management/commands/lightcurve_extraction.py
+++ neoexchange/core/management/commands/lightcurve_extraction.py
@@ -50,7 +50,7 @@ class Command(BaseCommand):
         except Block.DoesNotExist:
             self.stdout.write("Cannot find Block# %d" % options['blocknum'])
             exit(-1)
-        
+
         self.stdout.write("Analyzing Block# %d for %s" % (block.id, block.body.current_name()))
 
         frames = Frame.objects.filter(block=block.id, zeropoint__isnull=False, frametype__in=[Frame.BANZAI_QL_FRAMETYPE, Frame.BANZAI_RED_FRAMETYPE]).order_by('midpoint')
@@ -93,15 +93,24 @@ class Command(BaseCommand):
                         mags.append(best_source.obs_mag)
                         mag_errs.append(best_source.err_obs_mag)
 
+            self.stdout.write("Found matches in %d of %d frames" % ( len(times), len(frames)))
+
+            # Write light curve data out in similar format to Make_lc.csh
             i=0
             lightcurve_file = open('lightcurve_data.txt', 'w')
+
+            # Calculate integer part of JD for first frame and use this as a
+            # constant in case of wrapover to the next day
+            mjd_offset = int(datetime2mjd_utc(times[0]))
             for time in times:
                 time_jd = datetime2mjd_utc(time)
-                time_jd_truncated = time_jd - int(time_jd)
+                time_jd_truncated = time_jd - mjd_offset
+                if i == 0:
+                    lightcurve_file.write("#MJD-%.1f Mag. Mag. error\n" % (mjd_offset))
                 lightcurve_file.write( "%7.5lf %6.3lf %5.3lf\n" % ( time_jd_truncated, mags[i], mag_errs[i] ) )
                 i += 1
             lightcurve_file.close()
-  
+
             if options['title'] == None:
                 plot_title = '%s from %s (%s) on %s' % (block.body.current_name(), block.site.upper(), frame.sitecode, block.when_observed.strftime("%Y-%m-%d"))
             else:
