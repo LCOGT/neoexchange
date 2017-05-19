81a165f313b8f13099d65709ff1cef4a47edc3d1
Jessica MacFarlane <jlmacfarlane@pipeline.sbcc.edu>
2017-05-19 16:53:50 -0700

    The code has now created one point on a plot.

--- neoexchange/astrometrics/albedo.py
+++ neoexchange/astrometrics/albedo.py
@@ -4,38 +4,41 @@ import numpy as np
 import matplotlib.pyplot as plt
 from astropy.modeling import models, fitting 
 import random
+import math
 
 #this will generate the x-axis number
-def albedo(x):
 
-    x = random.random()
-    y = random.random()
+x = random.random()
+y = random.random()
     
-    f = 0.253
-    bright = 0.0168
-    dark = 0.03  
+f = 0.253
+bright = 0.0168
+dark = 0.03  
     
-    if x < f:
-        t = dark
+if x < f:
+    t = dark
 
-    else:
-        t = bright
+else:
+    t = bright
 
-    a = t * math.sqrt(-2 * math.log1p(1 - y))
-    return a
+#this is the x and y numbers to be plot. the equation was two fractions so I typed out the numerators to make things easier later and the inside of the sqrt
+inside = -2 * math.log(1 - y) 
+print inside, y
 
-print a
+a = t * math.sqrt(inside)
+top1 = a * math.exp(-a ** 2 / 2 * dark ** 2)
+top2 = a * math.exp(-a ** 2 / 2 * bright **2)
+prr = f * (top1 / dark ** 2) + (1 - f) * (top2 / bright ** 2)
 
-#this is the start of the plot for the random data generated in the function above (using astropy.modeling)
-gg_init = models.Gaussian1D(2.5,0.15,0.1) + models.Gaussian1D(6,0.05,0.1)
-fitter = fitting.SLSQPLSQFitter()
-gg_fit = fitter(gg_init, x, y)
-
+#this should plot the points 
 plt.figure(figsize=(8,7))
-plt.plot(x, y, 'ko')
-plt.plot(x, gg_fit(x))
+plt.plot(a, prr, 'ko')
 plt.xlabel('Pv')
 plt.ylabel('P(Pv)')
 
+plt.show()
 
-def diameter
+
+
+
+
