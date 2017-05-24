e72574d87363c5e15105b71caccc77b31e1ea707
Jessica MacFarlane <jlmacfarlane@pipeline.sbcc.edu>
2017-05-23 17:09:56 -0700

    Working plot of Albedo Distribution

--- neoexchange/astrometrics/albedo.py
+++ neoexchange/astrometrics/albedo.py
@@ -2,43 +2,47 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-from astropy.modeling import models, fitting 
+import matplotlib.mlab as mlab
+from astropy.modeling import models, fitting
 import random
 import math
 
+a_data = []
 #this will generate the x-axis number
 
-x = random.random()
-y = random.random()
-    
-f = 0.253
-bright = 0.0168
-dark = 0.03  
-    
-if x < f:
-    t = dark
+for i in range(0, 50000):   
+	x = random.random()
+	y = random.random()
+	
+	f = 0.253
+	bright = 0.168
+	dark = 0.03  
 
-else:
-    t = bright
+	if x < f:
+		t = dark
+
+	else:
+		t = bright
 
 #this is the x and y numbers to be plot. the equation was two fractions so I typed out the numerators to make things easier later and the inside of the sqrt
-inside = -2 * math.log(1 - y) 
-print inside, y
+	inside = -2.0 * (math.log(1.0 - y)) 
+	a = t * math.sqrt(inside)
+	a_data.append(a)
 
-a = t * math.sqrt(inside)
-top1 = a * math.exp(-a ** 2 / 2 * dark ** 2)
-top2 = a * math.exp(-a ** 2 / 2 * bright **2)
-prr = f * (top1 / dark ** 2) + (1 - f) * (top2 / bright ** 2)
 
-#this should plot the points 
-plt.figure(figsize=(8,7))
-plt.plot(a, prr, 'ko')
-plt.xlabel('Pv')
-plt.ylabel('P(Pv)')
+#this should plot the points
+mu = 1.5
+sigma = 0.027
 
+num_bins = 100
+fig, ax = plt.subplots()
+
+n, bins, patches = ax.hist(a_data, num_bins)
+
+ax.set_xlabel('Pv')
+ax.set_ylabel('P(Pv)')
+ax.set_title(r'Near Earth Asteroid Albedo Distribution') 
+
+fig.tight_layout()
 plt.show()
-
-
-
-
-
+		
