e2ed6114fe10bf8d2291be4003b071a01c0fc249
Jessica MacFarlane <jlmacfarlane@pipeline.sbcc.edu>
2017-06-02 16:39:56 -0700

    asteroid_diameter now returns values in meters and albedo is now called asteroid_albedo

--- neoexchange/astrometrics/albedo.py
+++ neoexchange/astrometrics/albedo.py
@@ -8,7 +8,7 @@ from astropy.modeling import fitting
 import random
 import math
 
-def albedo(f=0.253, bright=0.168, dark=0.03):
+def asteroid_albedo(f=0.253, bright=0.168, dark=0.03):
     '''This function generates a single albedo. It takes a dark fraction(f),      
      a dark peak(dark), and a bright peak(bright) and returns a value of albedo    
      for the given numbers.
@@ -28,7 +28,7 @@ def albedo(f=0.253, bright=0.168, dark=0.03):
     return albedo
 
 
-def albedo_distribution(f=0.253, bright=0.168, dark=0.03, a=albedo()):
+def albedo_distribution(f=0.253, bright=0.168, dark=0.03, a=asteroid_albedo()):
     '''This function generates a single albedo distribution. It takes a dark 
     fraction(f), a dark peak(dark), a bright peak(bright), and an albedo(a) and 
     returns a value of albedo for the given numbers. Note that to use the default 
@@ -42,17 +42,17 @@ def albedo_distribution(f=0.253, bright=0.168, dark=0.03, a=albedo()):
     else: 
         return 'Check your albedo!'
 
-def asteroid_diameter(a=albedo(), h=7):
+def asteroid_diameter(a=asteroid_albedo(), h=7):
     '''This function calculates the diameter of an asteroid. It takes an albedo(a) 
     and a H magnitude(h). Note that to use the default you must also use the 
     method 'albedo' which generates a random albedo. Also note that the diameter 
-    returned is in kilometers. 
+    returned is in meters. 
     See Wright et al. The Astronomical Journal, 2016, 152, 79'''
     
     if a <= 0.00:
         return 'You cannot have a negative albedo!'
     else:
-        diameter = 1329 * math.sqrt(10 ** (-0.4 * h) / a)
+        diameter = 1329000 * math.sqrt((10 ** (-0.4 * h)) / a)
         return diameter
 
 if __name__ == '__main__':
@@ -60,30 +60,54 @@ if __name__ == '__main__':
 #this will generate the x-axis number
     a_data = []
     prr_data = []
-    dia_data = []
-    fig, ax = plt.subplots()
+    dia_data1 = []
+    dia_data2 = []
+    dia_data3 = []
+    fig, ax2 = plt.subplots()
 
-    for i in range(0, 100000):
-        a1 = albedo()
+    for i in range(0, 100):
+        a1 = asteroid_albedo()
         a_data.append(a1)
         prr = albedo_distribution(a=a1)
         prr_data.append(prr)
-        #di = asteroid_diameter(a=a1, h=20)
-        #dia_data.append(di)
+        di1 = asteroid_diameter(a=a1, h=17.75)
+        dia_data1.append(di1)
+        di2 = asteroid_diameter(a=a1, h=20.0)
+        dia_data2.append(di2)
+        di3 = asteroid_diameter(a=a1, h=22.5)
+        dia_data3.append(di3)
+        
     
     #this should plot the points
     num_bins = 100
     
-    n, bins, patches = ax.hist(a_data, num_bins, normed=1, label='Albedo Distribution')
-    plt.plot(a_data, prr_data,'k.', label='Albedo Distribution Best Fit')
-    #plt.plot(a_data, dia_data,'g.', label='Diameter based on Albedo')
+    #n, bins, patches = ax1.hist(a_data, num_bins, normed=1, label='Albedo Distribution')
+    #ax1.plot(a_data, prr_data,'k.', label='Albedo Distribution Best Fit')
+    ax2.plot(a_data, dia_data1,'g.', label='Magnitude = 17.75')
+    ax2.plot(a_data, dia_data2, 'r.', label='Magnitude = 20.0')
+    ax2.plot(a_data, dia_data3, 'y.', label='Magnitude = 22.5')
     
-    ax.set_xlabel('Pv')
-    ax.set_ylabel('P(Pv)')
-    ax.set_title(r'Near Earth Asteroid Albedo Distribution') 
-    ax.legend(loc='upper right')
+    #ax1.set_xlabel('Pv')
+    #ax1.set_ylabel('P(Pv)')
+    ax2.set_xlabel('Pv')
+    ax2.set_ylabel('Diameter (m)')
+    #ax1.set_title(r'Near Earth Asteroid Albedo Distribution')
+    ax2.set_title(r'Near Earth Asteroid Diameter and Albedo') 
+    #ax1.legend(loc='upper right')
+    ax2.legend(loc='upper right')
 
     fig.tight_layout()
-    plt.show()
+  #  plt.show()
+    
 
-
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
