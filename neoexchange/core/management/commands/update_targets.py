ec7605bd6fe1163de741935aa459e6f767b54c26
Jessica MacFarlane <jlmacfarlane@pipeline.sbcc.edu>
2017-06-29 11:35:47 -0700

    Still needs updating but now when update_targets is called it prints 'Updating Targets...' line

--- neoexchange/core/management/commands/update_targets.py
+++ neoexchange/core/management/commands/update_targets.py
@@ -21,7 +21,7 @@ class Command(BaseCommand):
             default='objects',
             nargs='?',
             type=str,
-            help='Updates objects from radar observatories origins'
+            help='Updates objects from specific origins. "objects" updates all but MPC and LCO objects, "allneos" updates all asteroids, "nasa" updates NASA affiated origins, and "radar" updates targets from radar'
         )
         
         parser.add_argument(
@@ -33,18 +33,40 @@ class Command(BaseCommand):
             nargs='?',            
             help='Updates objects depending on number of hours past the objects original update'
         )
-         
+
+
+    def handle(self, *args, **options):
+        try:
+            if options['allneos']:
+                allneos = ['M', 'N', 'S', 'D', 'G', 'A', 'R', 'L']
+                self.update_neos(origins=allneos)
+            elif options['objects']:
+                objects = ['N', 'S', 'D', 'G', 'A', 'R']
+                self.update_neos(origins=objects)
+            elif options['nasa']:
+                nasa = ['G', 'N']
+                self.update_neos(origins=nasa)
+            elif options['radar']:
+                radar = ['G','A','R']
+                self.update_neos(origins=radar)
+            else:
+                objects = ['N', 'S', 'D', 'G', 'A', 'R']
+                self.update_neos(origins=objects)    
+        except KeyError:
+            objects = ['N', 'S', 'D', 'G', 'A', 'R']
+            self.update_neos(origins=objects)
+                    
             
-    def update_neos(origins=['N', 'S', 'D', 'G', 'A', 'R']):
+    def update_neos(self, origins=['N', 'S', 'D', 'G', 'A', 'R']):
         self.stdout.write("==== Updating Targets %s ====" % (datetime.now().strftime('%Y-%m-%d %H:%M')))
-        targets = Body.objects.filter(origin=origins).order_by('-ingest')
-        print "I made it"
+        targets = Body.objects.get(origin=origins).order_by('-ingest')
+        
         if targets.updated == False:
             self.stdout.write("Reading NEO %s from %s" % targets.name, targets.origin)
             update_MPC_orbit(targets.name, targets.origin)
             # Wait between 10 and 20 seconds
             delay = random_delay(10, 20)
-            print "Here"
+            
         elif targets.updated == True:
         #checks when it has been last updated
     
@@ -54,31 +76,14 @@ class Command(BaseCommand):
                 update_MPC_orbit(targets.name, targets.origin)
                 # Wait between 10 and 20 seconds
                 delay = random_delay(10, 20)
-                print "There"
+                
             elif targets.update_time >= datetime.now() - timedelta(hours=48):
             #if object has not been updated in 48 hours
                 self.stdout.write("Reading NEO %s from %s" % targets.name, targets.origin)
                 update_MPC_orbit(targets.name, targets.origin)
                 # Wait between 10 and 20 seconds
                 delay = random_delay(10, 20)
-                print "Here again"
-                    
+                
+        else:
+            self.stdout.write("No NEOs to be updated")
 
-    def handle(self, *args, **options):
-            if options['allneos']:
-                allneos = ['M', 'N', 'S', 'D', 'G', 'A', 'R', 'L']
-                update_neos(origins=allneos)
-            elif options['objects']:
-                objects = ['N', 'S', 'D', 'G', 'A', 'R']
-                update_neos(origins=objects)
-            elif options['nasa']:
-                nasa = ['G', 'N']
-                update_neos(origins=nasa)
-            elif options['radar']:
-                radar = ['G','A','R']
-                update_neos(origins=radar)
-            else:
-                objects = ['N', 'S', 'D', 'G', 'A', 'R']
-                update_neos(origins=objects)
-        
-        
