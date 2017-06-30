c90169d5dc5eaaddd5440ee9c496b530f12890b2
Jessica MacFarlane <jlmacfarlane@pipeline.sbcc.edu>
2017-06-30 16:59:25 -0700

    Update_neos is running through but the handle method is still not working. The arguments are falling through the if statements.

--- neoexchange/core/management/commands/update_targets.py
+++ neoexchange/core/management/commands/update_targets.py
@@ -15,10 +15,28 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument(
-            'origin',
-            choices=['objects', 'allneos', 'nasa', 'radar'],
-            const='objects',
-            default='objects',
+            'objects',
+            nargs='?',
+            type=str,
+            help='Updates objects from specific origins. "objects" updates all but MPC and LCO objects, "allneos" updates all asteroids, "nasa" updates NASA affiated origins, and "radar" updates targets from radar'
+        )
+        
+        parser.add_argument(
+            'allneos',
+            nargs='?',
+            type=str,
+            help='Updates objects from specific origins. "objects" updates all but MPC and LCO objects, "allneos" updates all asteroids, "nasa" updates NASA affiated origins, and "radar" updates targets from radar'
+        )
+        
+        parser.add_argument(
+            'nasa',
+            nargs='?',
+            type=str,
+            help='Updates objects from specific origins. "objects" updates all but MPC and LCO objects, "allneos" updates all asteroids, "nasa" updates NASA affiated origins, and "radar" updates targets from radar'
+        )
+        
+        parser.add_argument(
+            'radar',
             nargs='?',
             type=str,
             help='Updates objects from specific origins. "objects" updates all but MPC and LCO objects, "allneos" updates all asteroids, "nasa" updates NASA affiated origins, and "radar" updates targets from radar'
@@ -36,55 +54,54 @@ class Command(BaseCommand):
 
 
     def handle(self, *args, **options):
+        time = options['time']
+        
         try:
             if options['allneos']:
-                allneos = ['M', 'N', 'S', 'D', 'G', 'A', 'R', 'L']
-                self.update_neos(origins=allneos)
-            elif options['objects']:
-                objects = ['N', 'S', 'D', 'G', 'A', 'R']
-                self.update_neos(origins=objects)
+                origins = ['M', 'N', 'S', 'D', 'G', 'A', 'R', 'L']
+                print "ALL OF THE NEOS!!!!"
             elif options['nasa']:
-                nasa = ['G', 'N']
-                self.update_neos(origins=nasa)
+                origins = ['G', 'N']
+                print "yes I do like nasa"
             elif options['radar']:
-                radar = ['G','A','R']
-                self.update_neos(origins=radar)
+                origins = ['G','A','R']
+                print "I am going through radar"
+            elif options['objects']:
+                origins = ['N', 'S', 'D', 'G', 'A', 'R']
+                print "not MPC not LCO"
             else:
-                objects = ['N', 'S', 'D', 'G', 'A', 'R']
-                self.update_neos(origins=objects)    
+                origins = ['N', 'S', 'D', 'G', 'A', 'R']
+                print "I've tried everything" 
         except KeyError:
-            objects = ['N', 'S', 'D', 'G', 'A', 'R']
-            self.update_neos(origins=objects)
+            origins = ['N', 'S', 'D', 'G', 'A', 'R']
+            
+        self.update_neos(origins, time)
                     
             
-    def update_neos(self, origins=['N', 'S', 'D', 'G', 'A', 'R']):
+    def update_neos(self, origins=['N', 'S', 'D', 'G', 'A', 'R'], time=12):
+        """Origins are the list of origins to be updated. The default list contains every origin except MPC and LCO. Time is in hours and its default is set to 12 hours."""
         self.stdout.write("==== Updating Targets %s ====" % (datetime.now().strftime('%Y-%m-%d %H:%M')))
-        targets = Body.objects.filter(origin=origins)
-        
-        if targets.filter(updated=False):
-            self.stdout.write("Reading NEO %s from %s" % targets.name, targets.origin)
-            update_MPC_orbit(targets.name, targets.origin)
-            # Wait between 10 and 20 seconds
-            delay = random_delay(10, 20)
+        targets = Body.objects.filter(origin__in=origins, active=True)
+        self.stdout.write("Length of target query set %d" %(targets.count()))
+        for target in targets:
+            time_now = datetime.utcnow()
+            if target.updated == False:
+                self.stdout.write("Reading NEO %s from %s" % target.name, target.origin)
+                update_MPC_orbit(target.name, target.origin)
+                # Wait between 10 and 20 seconds
+                delay = random_delay(10, 20)
             
-        elif targets.filter(updated=True):
-        #checks when it has been last updated
-
-            if targets.update_time >= datetime.now() - timedelta(hours=options['time']) and targets.update_time <= datetime.now() - timedelta(hours=24):
-            #if object has not been updated within 
-                self.stdout.write("Reading NEO %s from %s" % targets.name, targets.origin)
-                update_MPC_orbit(targets.name, targets.origin)
+            elif target.updated == True and target.update_time >= time_now - timedelta(hours=time) and target.update_time <= time_now - timedelta(hours=24):
+                #checks when it has been last updated
+                self.stdout.write("==== Checking Previously Updated Targets ====")
+                self.stdout.write("Reading NEO %s from %s" % target.name, target.origin)
+                update_MPC_orbit(target.name, target.origin)
                 # Wait between 10 and 20 seconds
                 delay = random_delay(10, 20)
+                     
+            else:
+                pass
                 
-            elif targets.update_time >= datetime.now() - timedelta(hours=48):
-                #if object has not been updated in 48 hours
-                self.stdout.write("Reading NEO %s from %s" % targets.name, targets.origin)
-                update_MPC_orbit(targets.name, targets.origin)
-                # Wait between 10 and 20 seconds
-                delay = random_delay(10, 20)
-                
-        else:
-            self.stdout.write("No NEOs to be updated")
+        self.stdout.write("==== No NEOs to be updated ====")
                
                     
