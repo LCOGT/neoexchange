c03a63d9cd70b2e0ad1d85ad7da25105d0866695
Jessica MacFarlane <jlmacfarlane@pipeline.sbcc.edu>
2017-07-07 12:29:51 -0700

    Arguments are now being passed into function. Problems are now with comparing time while trying to decide what to update...

--- neoexchange/core/management/commands/update_targets.py
+++ neoexchange/core/management/commands/update_targets.py
@@ -15,66 +15,41 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument(
-            'objects',
-            nargs='?',
+            '--sources',
             type=str,
-            help='Updates objects from specific origins. "objects" updates all but MPC and LCO objects, "allneos" updates all asteroids, "nasa" updates NASA affiated origins, and "radar" updates targets from radar'
+            choices=['allneos', 'nasa', 'radar', 'objects'],
+            default='objects',
+            help='Updates NEOs by origin source. "allneos" updates all NEOs, "nasa" updates origins assosiated with NASA, "radar" updates NEOs being followed by radar origins, "objects" updates all but MPC and LCO objects.'
         )
-        
+
         parser.add_argument(
-            'allneos',
-            nargs='?',
-            type=str,
-            help='Updates objects from specific origins. "objects" updates all but MPC and LCO objects, "allneos" updates all asteroids, "nasa" updates NASA affiated origins, and "radar" updates targets from radar'
-        )
-        
-        parser.add_argument(
-            'nasa',
-            nargs='?',
-            type=str,
-            help='Updates objects from specific origins. "objects" updates all but MPC and LCO objects, "allneos" updates all asteroids, "nasa" updates NASA affiated origins, and "radar" updates targets from radar'
-        )
-        
-        parser.add_argument(
-            'radar',
-            nargs='?',
-            type=str,
-            help='Updates objects from specific origins. "objects" updates all but MPC and LCO objects, "allneos" updates all asteroids, "nasa" updates NASA affiated origins, and "radar" updates targets from radar'
-        )
-        
-        parser.add_argument(
-            'time',
+            '--time',
             type=int,
             choices=range(1, 25),
-            const = 12,
-            default=12,
-            nargs='?',            
+            default=12,           
             help='Updates objects depending on number of hours past the objects original update'
         )
 
 
     def handle(self, *args, **options):
-        time = options['time']
-        
-        try:
-            if options['allneos']:
-                origins = ['M', 'N', 'S', 'D', 'G', 'A', 'R', 'L']
-                print "ALL OF THE NEOS!!!!"
-            elif options['nasa']:
-                origins = ['G', 'N']
-                print "yes I do like nasa"
-            elif options['radar']:
-                origins = ['G','A','R']
-                print "I am going through radar"
-            elif options['objects']:
-                origins = ['N', 'S', 'D', 'G', 'A', 'R']
-                print "not MPC not LCO"
-            else:
-                origins = ['N', 'S', 'D', 'G', 'A', 'R']
-                print "I've tried everything" 
-        except KeyError:
+        time = options['time'] 
+
+        if options['sources']=='allneos':
+            origins = ['M', 'N', 'S', 'D', 'G', 'A', 'R', 'L']
+            print "ALL THE NEOS!!!!"
+        elif options['sources']=='nasa':
+            origins = ['G', 'N']
+            print "yes I do like nasa"
+        elif options['sources']=='radar':
+            origins = ['G','A','R']
+            print "I am going through radar"
+        elif options['sources']=='objects':
             origins = ['N', 'S', 'D', 'G', 'A', 'R']
-            
+            print "not MPC not LCO"
+        else:
+            origins = ['N', 'S', 'D', 'G', 'A', 'R']
+            print "I've tried everything"         
+	   
         self.update_neos(origins, time)
                     
             
@@ -84,24 +59,30 @@ class Command(BaseCommand):
         targets = Body.objects.filter(origin__in=origins, active=True)
         self.stdout.write("Length of target query set %d" %(targets.count()))
         for target in targets:
+            print target
             time_now = datetime.utcnow()
+            timediff = timedelta(hours=time)
+            oneday = timedelta(hours=24)
             if target.updated == False:
-                self.stdout.write("Reading NEO %s from %s" % target.name, target.origin)
-                update_MPC_orbit(target.name, target.origin)
+                self.stdout.write("Reading NEO {name} from {origin}".format(name=target.name, origin=target.origin))
+                #update_MPC_orbit(target.name, target.origin)
                 # Wait between 10 and 20 seconds
-                delay = random_delay(10, 20)
-            
-            elif target.updated == True and target.update_time >= time_now - timedelta(hours=time) and target.update_time <= time_now - timedelta(hours=24):
+                #delay = random_delay(10, 20)
+                
+            elif target.updated == True and time_now - target.update_time >= time_now - timediff and time_now - target.update_time <= time_now - oneday:
                 #checks when it has been last updated
                 self.stdout.write("==== Checking Previously Updated Targets ====")
-                self.stdout.write("Reading NEO %s from %s" % target.name, target.origin)
-                update_MPC_orbit(target.name, target.origin)
+                self.stdout.write("Reading NEO {name} from {origin}".format(name=target.name, origin=target.origin))
+                
+                #update_MPC_orbit(target.name, target.origin)
                 # Wait between 10 and 20 seconds
-                delay = random_delay(10, 20)
+                #delay = random_delay(10, 20)
                      
             else:
                 pass
                 
         self.stdout.write("==== No NEOs to be updated ====")
+
+
                
                     
