91fd18a92aa89d7bf24967ded0d2f3701e8d7ff3
Jessica MacFarlane <jlmacfarlane@pipeline.sbcc.edu>
2017-07-14 15:57:33 -0700

    This code would pull information from NASA's SBDB on objects that make close approaches

---
+++ neoexchange/astrometrics/nasa_sbdb_closeapproach.py
@@ -0,0 +1,11 @@
+"""This code is to get information from NASA's JPL SBDB Close-Approach Data. This will produce a JSON file that can be manipulated."""
+
+import urllib.request
+
+
+"""
+this gets Earth close-approach data for NEOs between the dates Jan. 1st, 1900 to Jan 1st, 2100 and is sorted by distance 
+"""
+NASA_SBDB_url = 'https://ssd-api.jpl.nasa.gov/cad.api?body=Earth&date-min=1900-01-01&date-max=2100-01-01&sort=dist'
+
+
