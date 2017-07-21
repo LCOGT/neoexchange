70d8bb08e06d50a8dedcd725eec0336fab192021
Jessica MacFarlane <jlmacfarlane@pipeline.sbcc.edu>
2017-07-21 13:58:50 -0700

    Added request to create a json file

--- neoexchange/astrometrics/nasa_sbdb_closeapproach.py
+++ neoexchange/astrometrics/nasa_sbdb_closeapproach.py
@@ -8,4 +8,7 @@ this gets Earth close-approach data for NEOs between the dates Jan. 1st, 1900 to
 """
 NASA_SBDB_url = 'https://ssd-api.jpl.nasa.gov/cad.api?body=Earth&date-min=1900-01-01&date-max=2100-01-01&sort=dist'
 
+files = {'file': ('sbdb_July2017.json', open('sbdb_July2017.json', )}
 
+sbdb_request = requests.get(NASA_SBDB_url)
+sbdb_request.json()
