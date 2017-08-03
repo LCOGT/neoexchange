b8c7d808ed5d1ed714987de085897d97b3c4aec4
Tim Lister <tlister@lcogt.net>
2017-08-03 11:42:11 -0700

    Not seen vmdline value should be converted to a float

--- neoexchange/core/management/commands/robo_scheduler.py
+++ neoexchange/core/management/commands/robo_scheduler.py
@@ -99,7 +99,7 @@ class Command(BaseCommand):
         spd_help = "South Polar Distance cutoff for S. Hemisphere (%.1f=%+.1f Dec)" % (spd_default, spd_default-90.0)
         parser.add_argument('--spd_cutoff', default=spd_default, type=float, help=spd_help)
         parser.add_argument('--speed_limit', default=speed_limit_default, type=float, help="Rate of motion limit ("+str(speed_limit_default)+")")
-        parser.add_argument('--not_seen', default=not_seen_default, help="Cutoff since object was seen ("+str(not_seen_default)+" days)")
+        parser.add_argument('--not_seen', default=not_seen_default, type=float, help="Cutoff since object was seen ("+str(not_seen_default)+" days)")
         parser.add_argument('--too', action="store_true", help="Whether to execute as disruptive ToO")
 
     def handle(self, *args, **options):
