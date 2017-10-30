7120b87180c6809b19b447bec9aba5da0705ed1b
Tim Lister <tlister@lcogt.net>
2017-10-30 16:54:01 +0000

    Add print output to show progress every 100 Blocks

--- neoexchange/core/management/commands/migrate_block_to_superblock.py
+++ neoexchange/core/management/commands/migrate_block_to_superblock.py
@@ -12,7 +12,10 @@ class Command(BaseCommand):
         msg = "Found %d Blocks to migrate" % (blocks.count())
         self.stdout.write(msg)
 
+	printcounter = 0
         for block in blocks:
+	    if printcounter % 100==0:
+		print "Migrated Block #%d" % printcounter
             cadence = False
             if 'cad' in block.groupid:
                 cadence = True
@@ -20,3 +23,4 @@ class Command(BaseCommand):
             block.save()
             block.superblock.cadence = cadence
             block.superblock.save()
+	    printcounter += 1
