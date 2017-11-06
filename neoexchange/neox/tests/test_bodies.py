585e1e711feb3482b487f7f3b5609d3433868243
Joey Chatelain <jchatelain@lco.global>
2017-11-06 15:07:46 -0800

    Created new functional test to look for spectral info

--- neoexchange/neox/tests/test_bodies.py
+++ neoexchange/neox/tests/test_bodies.py
@@ -46,3 +46,32 @@ class BodyDetailsTest(FunctionalTest):
                      u'ARC LENGTH (DAYS) '  + str(self.body.arc_length)]
         for line in testlines:
             self.check_for_row_in_table('id_followup', line)
+
+    def test_can_view_spectral_details(self):
+
+		# A new user comes along to the site
+        self.browser.get(self.live_server_url)
+
+        # She sees a link from the targets' name on the front page to a more
+        # detailed view.
+        link = self.browser.find_element_by_link_text('N999r0q')
+        body_url = self.live_server_url + reverse('target',kwargs={'pk':1})
+        self.assertIn(link.get_attribute('href'), body_url)
+
+        # She clicks the link and is taken to a page with the targets' details.
+        with self.wait_for_page_load(timeout=10):
+            link.click()
+        new_url = self.browser.current_url
+        self.assertEqual(str(new_url), body_url)
+
+        # She notices the page title has the name of the site and the header
+        # mentions the current target
+        self.assertIn(self.body.current_name() + ' details | LCO NEOx', self.browser.title)
+        header_text = self.browser.find_element_by_class_name('headingleft').text
+        self.assertIn('Object: ' + self.body.current_name(), header_text)
+
+        #She notices there is a section describing the object's spectral info
+        testlines = [u'TAXONOMIC TYPE ' + str(self.body.taxonom_class),]
+
+        for line in testlines:
+            self.check_for_row_in_table('id_spectralinfo', line)
