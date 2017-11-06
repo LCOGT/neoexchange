045c2b10f5c68c64c482ed619731b060f00e4d43
Joey Chatelain <jchatelain@lco.global>
2017-11-06 14:39:22 -0800

    Updated version number for new branch

--- neoexchange/neox/settings.py
+++ neoexchange/neox/settings.py
@@ -4,7 +4,7 @@
 import os, sys
 from django.utils.crypto import get_random_string
 
-VERSION = '1.9.0'
+VERSION = '3.0.0a'
 
 CURRENT_PATH = os.path.dirname(os.path.realpath(__file__))
 PRODUCTION = True if CURRENT_PATH.startswith('/var/www') else False
