17cd9ea6b566958c41a0ed22d0a0f0d6f10b7897
Joey Chatelain <jchatelain@lco.global>
2017-11-13 16:50:33 -0800

    Should now update taxonomy for new objects, while skipping old ones.
    update_taxonomy_data.py: management function that pulls taxonomy from internet and runs views.update_taxonomy to add to DB

---
+++ neoexchange/core/management/commands/update_taxonomy_data.py
@@ -0,0 +1,18 @@
+from astrometrics.sources_subs import fetch_taxonomy_page
+from core.views import update_taxonomy
+
+from django.core.management.base import BaseCommand, CommandError
+from datetime import datetime
+
+class Command(BaseCommand):
+    help = 'Check Database for objects in need of Taxonomy update'
+
+    def handle(self, *args, **options):
+        self.stdout.write("==== Fetching Taxonomy Tables %s ====" % (datetime.now().strftime('%Y-%m-%d %H:%M')))
+        new_tax_data = fetch_taxonomy_page()
+        for tax_id in new_tax_data:
+            resp = update_taxonomy(tax_id)
+            if resp:
+                msg = "Updated Taxonomy for %s" % tax_id[0]
+                self.stdout.write(msg)
+        
