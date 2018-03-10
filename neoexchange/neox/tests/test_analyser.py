49344f907ad196178bf3b0a9012d32c32b115e11
Joey Chatelain <jchatelain@lco.global>
2018-03-09 17:48:36 -0800

    Add candidate and fram details to test to successfully load candidate page

--- neoexchange/neox/tests/test_analyser.py
+++ neoexchange/neox/tests/test_analyser.py
@@ -1,4 +1,5 @@
 from .base import FunctionalTest
+from datetime import datetime
 from django.test import TestCase, override_settings
 from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
@@ -6,6 +7,8 @@ from selenium import webdriver
 from mock import patch
 from neox.tests.mocks import MockDateTime, mock_lco_authenticate, mock_find_images_for_block
 from core.models import Frame, SourceMeasurement, Candidate
+from astropy.wcs import WCS
+from numpy import array
 import time
 
 class AnalyserTest(FunctionalTest):
@@ -23,6 +26,7 @@ class AnalyserTest(FunctionalTest):
         self.marge.is_active=1
         self.marge.save()
 
+        null_wcs = WCS()
         params = {  'sitecode'      : 'K93',
                     'instrument'    : 'kb75',
                     'filter'        : 'w',
@@ -31,7 +35,8 @@ class AnalyserTest(FunctionalTest):
                     'midpoint'      : '2017-01-01 21:09:51',
                     'frametype'     : Frame.BANZAI_RED_FRAMETYPE,
                     'block'         : self.test_block,
-                    'frameid'       : 1
+                    'frameid'       : 1,
+                    'wcs'           : null_wcs
                  }
         self.frame1 = Frame.objects.create(**params)
         params = {  'sitecode'      : 'K93',
@@ -42,7 +47,8 @@ class AnalyserTest(FunctionalTest):
                     'midpoint'      : '2017-01-01 21:20:00',
                     'frametype'     : Frame.BANZAI_RED_FRAMETYPE,
                     'block'         : self.test_block,
-                    'frameid'       : 2
+                    'frameid'       : 2,
+                    'wcs'           : null_wcs
                  }
         self.frame2 = Frame.objects.create(**params)
         params1 = {
@@ -65,17 +71,33 @@ class AnalyserTest(FunctionalTest):
         self.test_block.num_observed = 1
         self.test_block.save()
 
+        # Build Candidate --- WHY??? This cannot be the best way...
+        self.dtypes =\
+             {  'names' : ('det_number', 'frame_number', 'sext_number', 'jd_obs', 'ra', 'dec', 'x', 'y', 'mag', 'fwhm', 'elong', 'theta', 'rmserr', 'deltamu', 'area', 'score', 'velocity', 'sky_pos_angle', 'pixels_frame', 'streak_length'),
+                'formats' : ('i4',       'i1',           'i4',          'f8',     'f8', 'f8', 'f4', 'f4', 'f4', 'f4',   'f4',    'f4',    'f4',     'f4',       'i4',   'f4',   'f4',       'f4',        'f4',           'f4' )
+             }
+
+        self.dets_array = array([(0001, 1, 3283, 2457444.656045, 10.924317, 39.27700, 2103.245, 2043.026, 19.26, 12.970, 1.764, -60.4, 0.27, 1.39, 34, 1.10, 0.497, 0.2, 9.0, 6.7),
+                                (0001, 2,    0, 2457444.657980, 10.924298, 39.27793, 2103.468, 2043.025,  0.00,  1.000, 1.000,   0.0, 0.27, 0.00,  0, 1.10, 0.497, 0.2, 9.0, 6.7),
+                                (0001, 3, 3409, 2457444.659923, 10.924271, 39.27887, 2104.491, 2043.034, 19.20, 11.350, 1.373, -57.3, 0.27, 1.38, 52, 1.10, 0.497, 0.2, 9.0, 6.7),
+                                (0001, 4, 3176, 2457444.661883, 10.924257, 39.27990, 2104.191, 2043.844, 19.01, 10.680, 1.163, -41.5, 0.27, 1.52, 52, 1.10, 0.497, 0.2, 9.0, 6.7),
+                                (0001, 5, 3241, 2457444.663875, 10.924237, 39.28087, 2104.365, 2043.982, 19.17, 12.940, 1.089, -31.2, 0.27, 1.27, 55, 1.10, 0.497, 0.2, 9.0, 6.7),
+                                (0001, 6, 3319, 2457444.665812, 10.924220, 39.28172, 2104.357, 2043.175, 18.82, 12.910, 1.254, -37.8, 0.27, 1.38, 69, 1.10, 0.497, 0.2, 9.0, 6.7),],
+                                dtype=self.dtypes)
+        self.dets_byte_array = self.dets_array.tostring()
         params3 = {
             'block'   : self.test_block,
-            'cand_id' : 1,
-            'score'   : 17.47,
-            'avg_midpoint': '2017-01-01 21:20:00',
-            'avg_x'     : 2029.354,
-            'avg_y'     : 2066.806,
-            'avg_ra'    : 18.6,
-            'avg_dec'   : -29.1,
-            'speed'     : 0.72,
-            'sky_motion_pa' : 286.2
+            'cand_id' : 0001,
+            'score'  : 1.42,
+            'avg_midpoint' : datetime(2016, 2, 26, 3, 53, 7),
+            'avg_x'  : 1024.0,
+            'avg_y'  : 1042.3,
+            'avg_ra' : 123.42,
+            'avg_dec' : -42.3,
+            'avg_mag' : 20.7,
+            'speed'   : 0.497,
+            'sky_motion_pa' : 90.4,
+            'detections' : self.dets_byte_array
         }
         self.candidate = Candidate.objects.create(**params3)
 
