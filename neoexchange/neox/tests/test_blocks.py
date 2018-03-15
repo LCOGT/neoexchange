3941a01b788c749794fecd9e087a437d0144497d
Tim Lister <tlister@lcogt.net>
2018-03-15 09:54:37 -0700

    Start of functional test for spectroscopic blocks

--- neoexchange/neox/tests/test_blocks.py
+++ neoexchange/neox/tests/test_blocks.py
@@ -198,3 +198,64 @@ class SuperBlockListValidationTest(FunctionalTest):
         self.assertIn('2015-04-20 03:31', kv_table_text)
         self.assertNotIn('Not Reported', kv_table_text)
         self.assertIn('2015-04-20 09:29', kv_table_text)
+
+class SpectroBlocksListValidationTest(FunctionalTest):
+
+    def insert_spectro_blocks(self):
+
+        sblock_params = {
+                         'cadence' : False,
+                         'body'     : self.body,
+                         'proposal' : self.test_proposal,
+                         'block_start' : '2015-04-20 13:00:00',
+                         'block_end'   : '2015-04-22 03:00:00',
+                         'tracking_number' : '4242',
+                         'active'   : True
+                       }
+        self.test_sblock = SuperBlock.objects.create(**sblock_params)
+
+        block_params = { 'telclass' : '2m0',
+                         'site'     : 'ogg',
+                         'body'     : self.body,
+                         'proposal' : self.test_proposal,
+                         'block_start' : '2015-04-20 13:00:00',
+                         'block_end'   : '2015-04-21 03:00:00',
+                         'tracking_number' : '12345',
+                         'num_exposures' : 1,
+                         'exp_length' : 1800.0,
+                         'active'   : True,
+                         'superblock' : self.test_sblock
+                       }
+        self.test_block = Block.objects.create(**block_params)
+
+    def test_can_view_blocks(self):
+        self.insert_spectro_blocks()
+
+        # A new user, Timo, comes along to the site
+        self.browser.get(self.live_server_url)
+
+        # He sees a link to 'active blocks'
+        link = self.browser.find_element_by_partial_link_text('active blocks')
+        target_url = "{0}{1}".format(self.live_server_url, '/block/list/')
+        actual_url = link.get_attribute('href')
+        self.assertEqual(actual_url, target_url)
+
+        # He clicks the link to go to the blocks page
+        with self.wait_for_page_load(timeout=20):
+            link.click()
+        actual_url = self.browser.current_url
+        self.assertEqual(actual_url, target_url)
+
+        # He notices the page title has the name of the site and the header
+        # mentions current targets
+        self.assertIn('Blocks | LCO NEOx', self.browser.title)
+        header_text = self.browser.find_element_by_class_name('headingleft').text
+        self.assertIn('Observing Blocks', header_text)
+
+        # He sees that there are both spectroscopic and non-spectroscopic Blocks scheduled.
+        self.check_for_header_in_table('id_blocks',
+            'Block # Target Name Site Telescope Type Proposal Tracking Number Obs. Details Cadence? Active? Observed? Reported?')
+        testlines = [u'1 N999r0q CPT 1m0 LCO2015A-009 00042 5x42.0 secs Yes Active 0 / 1 0 / 1',
+                     u'2 N999r0q COJ 2m0 LCOEngineering 00043 7x30.0 secs No Not Active 1 / 1 1 / 1',
+                     u'3 N999r0q OGG 2m0-Spec LCOEngineering 4242 1x1800.0 secs No Active 1 / 1 1 / 1']
+        self.check_for_row_in_table('id_blocks', testlines[2])
