0a92cacc647326821c6158f2cdbd8aff01daf66e
Joey Chatelain <jchatelain@lco.global>
2018-05-14 12:36:20 -0700

    Simplify HTTP error exception

--- neoexchange/astrometrics/sources_subs.py
+++ neoexchange/astrometrics/sources_subs.py
@@ -107,9 +107,7 @@ def fetchpage_and_make_soup(url, fakeagent=False, dbg=False, parser="html.parser
     opener = urllib.request.build_opener()  # create an opener object
     try:
         response = opener.open(req_page)
-    except urllib.error.URLError as e:
-        if not hasattr(e, "code"):
-            raise
+    except urllib.error.HTTPError as e:
         print("Page retrieval failed:", e)
         return None
 
