37230dc0b8624948485078f5027973af4ef14742
artedeschi <artedeschi@gmail.com>
2018-06-28 11:17:24 -0700

    Changed .fits reading algorithm. cleaned up get_x_units() function and other small things

--- neoexchange/photometrics/spectraplot.py
+++ neoexchange/photometrics/spectraplot.py
@@ -5,32 +5,24 @@ Date: 6/25/2018
 for NeoExchange
 """
 
-from astropy.io import fits
-from astropy.io import ascii
-from astropy.convolution import convolve, Box1DKernel
+from astropy.io import fits, ascii
+from astropy.convolution import convolve, Box1DKernel #Gaussian1DKernel
+from astropy.wcs import WCS
 from astropy import units as u
 import matplotlib.pyplot as plt
-import matplotlib.ticker as ticker
+#import matplotlib.ticker as ticker
 import numpy as np
 
 #np.set_printoptions(threshold=np.inf)
 
-def get_fits_units(hdul):
-    """finds wavelength units, smallest, and largest wavelengths from .fits file header
-       inputs: <hdul> HDUList of fits file
-       outputs: x_units, x_min, x_max
-    """
-    try:
-        x_min = hdul[0].header['XMIN']
-        x_max = hdul[0].header['XMAX']
-    except KeyError:
-        try:
-            x_min = hdul[0].header['WMIN']
-            x_max = hdul[0].header['WMAX']
-        except KeyError:
-            raise KeyError("Could not parse wavelength range from FITS header")
-        pass
-            
+def get_x_units(x_data):
+    """finds wavelength units from x_data
+       inputs: <xdata>: unitless wavelength data
+       outputs: x_units
+    """ 
+    #xdata should be ndarray type. Will try error handling for it later
+    x_min = np.amin(x_data)
+
     #assuming visible to NIR range (~3000-10000A)
     if x_min >1000:
         x_units = u.AA #(Angstroms)
@@ -39,53 +31,41 @@ def get_fits_units(hdul):
     elif .1 < x_min < 1:
         x_units = u.micron
     else:
-        print("Warning: Could not parse wavelength units from header. Assuming Angstoms")
+        print("Warning: Could not parse wavelength units from file. Assuming Angstoms")
         x_units = u.AA 
                
-    return x_units, x_min, x_max
-    
-def get_ascii_units(data):
-    """find wavelength units from ascii table data
-       inputs: <data> ascii data table
-       outputs: x_units
-    """
-    x_min = data['col1'][0] 
-    #assuming 1st data point is smallest wavelength (will change later)
-    #assuming visible to NIR range (~3000-10000A)
-    if x_min >1000:
-        x_units = u.AA
-    elif 100 < x_min < 1000:
-        x_units = u.PrefixUnit(nm)
-    elif .1 < x_min < 1:
-        x_units = u.micron
-    else:
-        print("Warning: Could not parse wavelength units from header. Assuming Angstoms")
-        x_units = u.AA 
     return x_units
-
+    
 def read_spectra(spectra_file):
     """reads in spectra file (currently only works with LCO standards)
        inputs: <spectra_file>: path and file name to spectra
        outputs: wavelength (Quantity type), flux, flux_error
     """ 
     if spectra_file.endswith('.fits'):   
-        hdul = fits.open(spectra_file) 
+        hdul = fits.open(spectra_file) #read in data
 
+        #print(hdul.info())
         data = hdul[0].data
-        #find units
-        xunits, xmin, xmax = get_fits_units(hdul)
+        hdr = hdul[0].header
+
+        flux = data.flatten()[:max(data.shape)] 
+        w = WCS(hdr, naxis=1,relax=False,fix=False)
+        x_data = w.wcs_pix2world(np.arange(len(flux)),0)[0]
+        x_units = get_x_units(x_data)
         
-        flux = np.array(data[0][0]) #putting data into ndarrays
-        xdata = np.array([i/len(flux)*(xmax-xmin) + 
-        xmin for i in range(len(flux))])
-        flux_error = np.array(data[3][0]) 
+        try:
+            flux_error = np.array(data[3][0]) 
+        except IndexError:
+            print("Could not parse error data")
+            flux_error = np.zeros()
 
     elif spectra_file.endswith('.ascii'):
-        data = ascii.read(spectra_file)
-        xunits = get_ascii_units(data)
-        xdata = np.array([n for n in data['col1']]) #converting tables to ndarrays
-        flux = np.array([n for n in data['col2']])
-        flux_error = np.array([n for n in data['col3']])
+        data = ascii.read(spectra_file) #read in data
+        #assuming 3 columns: wavelength, flux/reflectance, error
+        x_data = data['col1'] #converting tables to ndarrays
+        flux = data['col2']
+        flux_error = data['col3']
+        x_units = get_x_units(x_data)
               
     else:
         raise OSError("Invalid input file type")
@@ -94,8 +74,8 @@ def read_spectra(spectra_file):
     flux[np.logical_not(flux >= 0)] = np.nan
     flux_error[np.logical_not(flux_error >= 0)] = np.nan
     
-    wavelength = (xdata*xunits).to(u.AA) #convert all wavelengths to Angstroms
-    return wavelength, flux, flux_error
+    wavelength = (x_data*x_units).to(u.AA) #convert all wavelengths to Angstroms
+    return wavelength, flux, flux_error,x_units
         
 
 def smooth(ydata, window=20):
@@ -111,8 +91,8 @@ def smooth(ydata, window=20):
     if window < 3:
         return ydata
 
-    if window % 2 != 0:
-        window += 1
+    #if window % 2 != 0: I believe Box 1D kernel handles this already
+    #    window += 1
 
     return convolve(ydata, Box1DKernel(window)) #boxcar average data
 
@@ -145,19 +125,25 @@ if __name__== "__main__":
     sol_ref = 'Solar_analogs/HD209847/nttHD209847_ftn_20180625_merge_2.0_58295_2_2df_ex.fits'
     
     window = 20 # 2 for eros ascii file. 20 for most others
-    x,y,yerr = read_spectra(path+spectra) 
+    x,y,yerr,x_units = read_spectra(path+spectra) 
     ysmoothed = smooth(y,window)
+
+    #Smoothing may cause artifacts at data ends.
+    #consider only plotting x[window:-window] and ysmoothed[window:-window]
     
     window_ref = 20
-    x_ref,y_ref,yerr_ref = read_spectra(path+sol_ref)
+    x_ref,y_ref,yerr_ref,x_ref_units = read_spectra(path+sol_ref)
     y_refsmoothed = smooth(y_ref, window_ref)
     
-    normy, normyerr = normalize(x,ysmoothed,yerr)
+    #print(x.shape, y.shape, x_ref.shape, y_ref.shape, ysmoothed.shape, y_refsmoothed.shape)
+
+    normy, normyerr = normalize(x,ysmoothed,yerr) #normalizing data
     normy_ref,normerr_ref = normalize(x_ref,y_refsmoothed,yerr_ref)
     
     #print(yerr)
     #print(normyerr)
     
+    #plotting data
     plot_spectra(x,normy)
     plot_spectra(x_ref,normy_ref)
     plt.show()
