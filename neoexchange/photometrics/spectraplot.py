207b14d2365d318fe1c1d39da0ea480f5eb00b8c
artedeschi <artedeschi@gmail.com>
2018-06-27 17:16:18 -0700

    cleaned a few things up

--- neoexchange/photometrics/spectraplot.py
+++ neoexchange/photometrics/spectraplot.py
@@ -9,7 +9,6 @@ from astropy.io import fits
 from astropy.io import ascii
 from astropy.convolution import convolve, Box1DKernel
 from astropy import units as u
-import matplotlib as mpl
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 import numpy as np
@@ -17,7 +16,10 @@ import numpy as np
 #np.set_printoptions(threshold=np.inf)
 
 def get_fits_units(hdul):
-    """finds xmin, xmax, and units DO REST LATER"""
+    """finds wavelength units, smallest, and largest wavelengths from .fits file header
+       inputs: <hdul> HDUList of fits file
+       outputs: x_units, x_min, x_max
+    """
     try:
         x_min = hdul[0].header['XMIN']
         x_max = hdul[0].header['XMAX']
@@ -26,11 +28,12 @@ def get_fits_units(hdul):
             x_min = hdul[0].header['WMIN']
             x_max = hdul[0].header['WMAX']
         except KeyError:
-            print("Could not parse wavelength range from FITS header")
-            raise  
+            raise KeyError("Could not parse wavelength range from FITS header")
+        pass
+            
     #assuming visible to NIR range (~3000-10000A)
     if x_min >1000:
-        x_units = u.AA
+        x_units = u.AA #(Angstroms)
     elif 100 < x_min < 1000:
         x_units = u.PrefixUnit(nm)
     elif .1 < x_min < 1:
@@ -42,6 +45,10 @@ def get_fits_units(hdul):
     return x_units, x_min, x_max
     
 def get_ascii_units(data):
+    """find wavelength units from ascii table data
+       inputs: <data> ascii data table
+       outputs: x_units
+    """
     x_min = data['col1'][0] 
     #assuming 1st data point is smallest wavelength (will change later)
     #assuming visible to NIR range (~3000-10000A)
@@ -66,8 +73,6 @@ def read_spectra(spectra_file):
 
         data = hdul[0].data
         #find units
-        #x_min = hdul[0].header['XMIN'] #sometimes "WMIN", sometimes "WMIN"
-        #x_max = hdul[0].header['XMAX'] #maybe put these two in their own functoin
         xunits, xmin, xmax = get_fits_units(hdul)
         
         flux = np.array(data[0][0]) #putting data into ndarrays
@@ -89,7 +94,7 @@ def read_spectra(spectra_file):
     flux[np.logical_not(flux >= 0)] = np.nan
     flux_error[np.logical_not(flux_error >= 0)] = np.nan
     
-    wavelength = (xdata*xunits).to(u.AA)
+    wavelength = (xdata*xunits).to(u.AA) #convert all wavelengths to Angstroms
     return wavelength, flux, flux_error
         
 
@@ -101,7 +106,7 @@ def smooth(ydata, window=20):
     """
 
     if len(ydata) < window:
-        raise ValueError("Input vector needs to be bigger than window size.")
+        raise ValueError("Input vector must be bigger than window size.")
 
     if window < 3:
         return ydata
@@ -111,46 +116,52 @@ def smooth(ydata, window=20):
 
     return convolve(ydata, Box1DKernel(window)) #boxcar average data
 
-def normalize(x,y,wavelength=5000*u.AA):
+def normalize(x,y,yerr,wavelength=5000*u.AA):
     """normalizes flux data with a specific wavelength flux value
-       inputs: <x>: wavelenth data
+       inputs: <x>: wavelenth data (Quantity type)
                <y>: flux data
-               [wavelength]: target wavelength to normalize at
+               <yerr>: 
+               [wavelength]: target wavelength to normalize at (Quantity type)
        outputs: normalized flux data
     """
-    normval = y[np.abs(x-wavelength).argmin()]
-    return y/normval
+    normval = y[np.abs(x-wavelength).argmin()] #uses closest data point to target wavelength
+    return y/normval,yerr/normval
 
 def plot_spectra(x,y):
     """plots spectra data
        imputs: <x>: wavelength data for x axis
                <y>: flux data for y axis
-       outputs:returns ax
+       outputs:returns ax DO LATER
     """
     plt.plot(x,y)
 
 if __name__== "__main__":
 
     path = '/home/atedeschi/test_spectra/' #will make more general file passing later
-    #spectra = '467309/20180613/ntt467309_U_ftn_20180613_merge_2.0_58283_1_2df_ex.fits'
-    spectra = 'calspec/eros_visnir_reference_to1um.ascii'
+    spectra = '467309/20180613/ntt467309_U_ftn_20180613_merge_2.0_58283_1_2df_ex.fits'
+    #spectra = 'calspec/eros_visnir_reference_to1um.ascii'
+    #spectra = 'calspec/sun_mod_001.fits'
 
     sol_ref = 'Solar_analogs/HD209847/nttHD209847_ftn_20180625_merge_2.0_58295_2_2df_ex.fits'
     
-    window = 2 #for eros ascii file. window = 20 for most others
-    x,y,y_err = read_spectra(path+spectra) 
+    window = 20 # 2 for eros ascii file. 20 for most others
+    x,y,yerr = read_spectra(path+spectra) 
     ysmoothed = smooth(y,window)
     
     window_ref = 20
-    x_ref,y_ref,y_err_ref = read_spectra(path+sol_ref)
+    x_ref,y_ref,yerr_ref = read_spectra(path+sol_ref)
     y_refsmoothed = smooth(y_ref, window_ref)
     
-    normy = normalize(x,ysmoothed)
-    normy_ref = normalize(x_ref,y_refsmoothed)
+    normy, normyerr = normalize(x,ysmoothed,yerr)
+    normy_ref,normerr_ref = normalize(x_ref,y_refsmoothed,yerr_ref)
+    
+    #print(yerr)
+    #print(normyerr)
     
     plot_spectra(x,normy)
     plot_spectra(x_ref,normy_ref)
     plt.show()
+    
 
 
 
