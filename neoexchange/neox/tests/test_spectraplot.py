6c62276df2ede1745a5ea8742a8d1fa88edd9431
Joey Chatelain <jchatelain@lco.global>
2018-08-16 16:37:21 -0700

    clean test_spectraplot

--- neoexchange/neox/tests/test_spectraplot.py
+++ neoexchange/neox/tests/test_spectraplot.py
@@ -1,3 +1,18 @@
+"""
+NEO exchange: NEO observing portal for Las Cumbres Observatory
+Copyright (C) 2014-2018 LCO
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+"""
+
 from .base import FunctionalTest
 from django.test import TestCase
 from django.core.urlresolvers import reverse
@@ -10,11 +25,12 @@ from neox.tests.mocks import MockDateTime, mock_lco_authenticate, mock_fetch_arc
 from django.conf import settings
 import os
 
+
 class SpectraplotTest(FunctionalTest):
 
         def setUp(self):
 
-            super(SpectraplotTest,self).setUp()
+            super(SpectraplotTest, self).setUp()
 
             settings.DATA_ROOT = os.getcwd()+'/photometrics/tests/'
 
@@ -22,9 +38,9 @@ class SpectraplotTest(FunctionalTest):
             self.password = 'simpson'
             self.email = 'bart@simpson.org'
             self.bart = User.objects.create_user(username=self.username, password=self.password, email=self.email)
-            self.bart.first_name= 'Bart'
+            self.bart.first_name = 'Bart'
             self.bart.last_name = 'Simpson'
-            self.bart.is_active=1
+            self.bart.is_active = 1
             self.bart.save()
 
             sblock_params = {
@@ -63,7 +79,6 @@ class SpectraplotTest(FunctionalTest):
                 'frameid'       : 1,
                }
             self.spec_frame = Frame.objects.create(**fparams)
-
 
             sblock2_params = {
                  'cadence'         : False,
@@ -117,7 +132,7 @@ class SpectraplotTest(FunctionalTest):
                  'exp_length'      : 1800.0,
                  'active'          : True,
                }
-            self.test_mblock1 = Block.objects.create(pk=6,**mblock1_params)
+            self.test_mblock1 = Block.objects.create(pk=6, **mblock1_params)
             mfparams1 = {
                 'sitecode'      : 'F65',
                 'filename'      : 'sp233/a265962.sp233.txt',
@@ -143,7 +158,7 @@ class SpectraplotTest(FunctionalTest):
                  'exp_length'      : 1800.0,
                  'active'          : True,
                }
-            self.test_mblock2 = Block.objects.create(pk=7,**mblock2_params)
+            self.test_mblock2 = Block.objects.create(pk=7, **mblock2_params)
             mfparams2 = {
                 'sitecode'      : 'F65',
                 'filename'      : 'sp233/a265962.sp233.txt',
@@ -189,7 +204,7 @@ class SpectraplotTest(FunctionalTest):
 
         @patch('neox.auth_backend.lco_authenticate', mock_lco_authenticate)
         @patch('core.archive_subs.fetch_archive_frames', mock_fetch_archive_frames)
-        def test_can_view_spectrum(self):   #test opening up a spectrum file associated with a block
+        def test_can_view_spectrum(self):   # test opening up a spectrum file associated with a block
             self.login()
             self.browser.get(self.live_server_url)
             blocks_url = reverse('blocklist')
@@ -198,8 +213,8 @@ class SpectraplotTest(FunctionalTest):
                 self.browser.find_element_by_link_text(str(self.test_sblock.pk)).click()
             with self.wait_for_page_load(timeout=10):
                 self.browser.find_element_by_link_text('Spectrum Plot').click()
-                #note: block and body do not match spectra.
-                #mismatch due to recycling and laziness
+                # note: block and body do not match spectra.
+                # mismatch due to recycling and laziness
             actual_url = self.browser.current_url
             target_url = self.live_server_url+'/block/'+str(self.test_block.pk)+'/spectra/'
             self.assertIn('Spectrum for block: '+str(self.test_block.pk)+' | LCO NEOx', self.browser.title)
@@ -207,7 +222,7 @@ class SpectraplotTest(FunctionalTest):
 
         @patch('neox.auth_backend.lco_authenticate', mock_lco_authenticate)
         @patch('core.archive_subs.fetch_archive_frames', mock_fetch_archive_frames)
-        def test_multi_spec_block(self):    #test opening 2 different spectra in same superblock
+        def test_multi_spec_block(self):    # test opening 2 different spectra in same superblock
             self.login()
             blocks_url = reverse('blocklist')
             self.browser.get(self.live_server_url + blocks_url)
@@ -223,7 +238,7 @@ class SpectraplotTest(FunctionalTest):
             self.browser.back()
             with self.wait_for_page_load(timeout=10):
                 self.browser.find_elements_by_link_text('Spectrum Plot')[1].click()
-            #note: this spectrum is same as first one. really just checking if pages are different.
+            # note: this spectrum is same as first one. really just checking if pages are different.
                 actual_url2 = self.browser.current_url
                 target_url2 = self.live_server_url+'/block/'+str(self.test_mblock2.pk)+'/spectra/'
             self.assertIn('Spectrum for block: '+str(self.test_mblock2.pk)+' | LCO NEOx', self.browser.title)
