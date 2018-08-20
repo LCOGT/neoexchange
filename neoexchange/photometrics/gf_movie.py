889428f0da86337af152269049bf0b74d68c008c
Joey Chatelain <jchatelain@lco.global>
2018-08-20 16:18:20 -0700

    clean gf_movie

--- neoexchange/photometrics/gf_movie.py
+++ neoexchange/photometrics/gf_movie.py
@@ -1,4 +1,5 @@
 """Creates a guide frame movie gif when given a series of guide frames"""
+
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
@@ -10,20 +11,22 @@ from astropy import units as u
 from glob import glob
 import os
 
+
 def make_gif(frames):
-""" takes in list of .fits guide frames and turns them into a moving gif.
+    """
+    takes in list of .fits guide frames and turns them into a moving gif.
     <frames> = list of .fits frames
     output = savefile (path of gif)
     """
-    files = np.sort(frames)
+    fits_files = np.sort(frames)
     dir = os.path.dirname(frames[0]).lstrip(' ')
 
-    hdu = fits.open(files[0])[0]
+    hdu = fits.open(fits_files[0])[0]
     if hdu.data:
             data = hdu.data
             header = hdu.header
     else:
-        hdu1 = fits.open(files[0])[1]
+        hdu1 = fits.open(fits_files[0])[1]
         data = hdu1.data
         header = hdu1.header
 
@@ -37,25 +40,26 @@ def make_gif(frames):
         site = 'FTN'
     else:
         site = 'FTS'
-    #wcs = WCS(header)
+    # wcs = WCS(header)
     interval = ZScaleInterval().get_limits(data)
 
     fig = plt.figure()
-    #fig.add_subplot(111,projection=wcs)
+    # fig.add_subplot(111,projection=wcs)
     fig.suptitle('Observation '+tn+' for '+obj+' at '+site)
 
-    anim = FuncAnimation(fig,update,frames=files,blit=True,interval=333) #takes in fig, update function, and frame rate set to 3fps
+    anim = FuncAnimation(fig, update, frames=fits_files, blit=True, interval=333)  # takes in fig, update function, and frame rate set to 3fps
 
     savefile = os.path.join(dir,'guidemovie.gif')
     anim.save(savefile, dpi=90, writer='imagemagick')
 
     return savefile
 
+
 def update(files):
-""" this method is requred to build FuncAnimation
+    """ this method is requred to build FuncAnimation
     <files> = frames to iterate through
     output: methods returned in this function are the things that are updated in each frame.
-"""
+    """
     hdu = fits.open(files)[0]
     if hdu.data:
             data = hdu.data
@@ -69,23 +73,23 @@ def update(files):
         date = header['DATE-OBS'][:-4]
     except KeyError:
         date = header['DATE_OBS'][:-4]
-    #wcs = WCS(header)
+    # wcs = WCS(header)
     interval = ZScaleInterval().get_limits(data)
     ax = plt.gca()
-    ax.set_title('Date: '+date)
-    ax.set_xlabel('RA Dec: '+header['RA']+ ' '+header['DEC'])
+    ax.set_title('Date: ' + date)
+    ax.set_xlabel('RA Dec: ' + header['RA'] + ' ' + header['DEC'])
     plt.tick_params(
-    axis='both',
-    which='both',
-    bottom=False,
-    left=False,
-    labelbottom=False,
-    labelleft=False)
+        axis='both',
+        which='both',
+        bottom=False,
+        left=False,
+        labelbottom=False,
+        labelleft=False)
+
+    return plt.imshow(data, cmap='gray', vmin=interval[0], vmax=interval[1]), ax.set_title('Date: '+date), ax.set_xlabel('RA Dec: '+header['RA']+' '+header['DEC'])
 
 
-    return plt.imshow(data,cmap='gray',vmin=interval[0],vmax=interval[1]),ax.set_title('Date: '+date),ax.set_xlabel('RA Dec: '+header['RA']+ ' '+header['DEC'])
-
-def test_display(file): #Leftover from debugging
+def test_display(file):  # Leftover from debugging
     hdu = fits.open(file)[0]
     if not any(hdu.data):
         hdu = fits.open(file)[1]
@@ -95,10 +99,11 @@ def test_display(file): #Leftover from debugging
     header = hdu.header
 
     interval = ZScaleInterval().get_limits(data)
-    plt.imshow(data,cmap='gray',vmin=interval[0],vmax=interval[1])
-    testname = os.path.join(dir,'test.png')
+    plt.imshow(data, cmap='gray', vmin=interval[0], vmax=interval[1])
+    testname = os.path.join(dir, 'test.png')
     plt.savefig(testname)
     return testname
+
 
 if __name__ == '__main__':
 
@@ -113,7 +118,6 @@ if __name__ == '__main__':
         data = hdu1.data
         header = hdu1.header
 
-
     minind = np.unravel_index(np.argmin(data, axis=None), data.shape)
     min = data[minind]
 
@@ -127,23 +131,23 @@ if __name__ == '__main__':
         site = 'FTN'
     else:
         site = 'FTS'
-    #wcs = WCS(header)
+    # wcs = WCS(header)
     interval = ZScaleInterval().get_limits(data)
 
     fig = plt.figure()
-    #fig.add_subplot(111,projection=wcs)
+    # fig.add_subplot(111,projection=wcs)
     fig.suptitle('Observation '+tn+' for '+obj+' at '+site)
-    image = plt.imshow(fits.getdata(files[0]),cmap='gray',vmin=interval[0],vmax=interval[1])
+    image = plt.imshow(fits.getdata(files[0]), cmap='gray', vmin=interval[0], vmax=interval[1])
     ax = plt.gca()
     ax.set_title('Date: '+date)
-    ax.set_xlabel('RA Dec: '+header['RA']+ ' '+header['DEC'])
+    ax.set_xlabel('RA Dec: '+header['RA']+' '+header['DEC'])
     plt.tick_params(
-    axis='both',
-    which='both',
-    bottom=False,
-    left=False,
-    labelbottom=False,
-    labelleft=False)
+        axis='both',
+        which='both',
+        bottom=False,
+        left=False,
+        labelbottom=False,
+        labelleft=False)
 
     # lon.set_major_formatter('hh:mm:ss')
     # lat.set_major_formatter('dd:mm:ss')
@@ -159,9 +163,9 @@ if __name__ == '__main__':
     # lat.set_ticklabel(fontsize=10)
     # ax.coords.grid(color='w',alpha =.5)
 
-    anim = FuncAnimation(fig,update,frames=np.arange(len(files)),blit=True,interval=333)
+    anim = FuncAnimation(fig, update, frames=np.arange(len(files)), blit=True, interval=333)
 
-    #plt.show()
+    # plt.show()
 
     savefile = dir+'guidemovie.gif'
     print('SAVE FILE: ', savefile)
