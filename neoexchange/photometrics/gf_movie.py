aaf4e802a5a2cfd8e7c9ead1ed84f0f887c0b43b
Joey Chatelain <jchatelain@lco.global>
2018-09-10 14:24:44 -0700

    add frame number and prevent RA clipping

--- neoexchange/photometrics/gf_movie.py
+++ neoexchange/photometrics/gf_movie.py
@@ -46,7 +46,52 @@ def make_gif(frames, title=None, sort=True, fr=333):
     if title:
         fig.suptitle(title)
 
-    anim = FuncAnimation(fig, update, frames=fits_files, blit=False, interval=fr)  # takes in fig, update function, and frame rate set to 3fps
+    def update(n):
+        """ this method is required to build FuncAnimation
+        <file> = frame currently being iterated
+        output: return plot.
+        """
+        # get data/Header from Fits
+        with fits.open(fits_files[n]) as hdul:
+            header = hdul['SCI'].header
+            data = hdul['SCI'].data
+        # pull Date from Header
+        try:
+            date_obs = header['DATE-OBS']
+        except KeyError:
+            date_obs = header['DATE_OBS']
+        date = datetime.strptime(date_obs, '%Y-%m-%dT%H:%M:%S.%f')
+        # reset plot
+        ax = plt.gca()
+        ax.clear()
+        ax.axis('off')
+        wcs = WCS(header)  # get wcs transformation
+        z_interval = ZScaleInterval().get_limits(data)  # set z-scale
+        # set wcs grid/axes
+        ax = plt.gca(projection=wcs)
+        dec = ax.coords['dec']
+        dec.set_major_formatter('dd:mm')
+        dec.set_ticks(exclude_overlapping=True)
+        dec.set_ticks_position('br')
+        dec.set_ticklabel_position('br')
+        dec.set_ticklabel(fontsize=10)
+        ra = ax.coords['ra']
+        ra.set_major_formatter('hh:mm:ss')
+        ra.set_ticks(exclude_overlapping=True)
+        ra.set_ticks_position('lb')
+        ra.set_ticklabel_position('lb')
+        ra.set_ticklabel(fontsize=10)
+        ax.coords.grid(color='black', ls='solid', alpha=0.5)
+        # finish up plot
+        ax.set_title('UT Date: {} ({} of {})'.format(date.strftime('%x %X'), n+1, len(fits_files)), pad=10)
+
+        plt.imshow(data, cmap='gray', vmin=z_interval[0], vmax=z_interval[1])
+        return ax
+
+    ax1 = update(0)
+    plt.tight_layout(pad=4)
+
+    anim = FuncAnimation(fig, update, frames=len(fits_files), blit=False, interval=fr)  # takes in fig, update function, and frame rate set to 3fps
 
     savefile = os.path.join(path, 'guidemovie.gif')
     anim.save(savefile, dpi=90, writer='imagemagick')
@@ -54,50 +99,5 @@ def make_gif(frames, title=None, sort=True, fr=333):
     return savefile
 
 
-def update(fits_file):
-    """ this method is required to build FuncAnimation
-    <file> = frame currently being iterated
-    output: return plot.
-    """
 
-    # get data/Header from Fits
-    with fits.open(fits_file) as hdul:
-        header = hdul['SCI'].header
-        data = hdul['SCI'].data
 
-    # pull Date from Header
-    try:
-        date_obs = header['DATE-OBS']
-    except KeyError:
-        date_obs = header['DATE_OBS']
-    date = datetime.strptime(date_obs, '%Y-%m-%dT%H:%M:%S.%f')
-
-    # reset plot
-    ax = plt.gca()
-    ax.clear()
-    ax.axis('off')
-
-    wcs = WCS(header)  # get wcs transformation
-    z_interval = ZScaleInterval().get_limits(data)  # set z-scale
-
-    # set wcs grid/axes
-    ax = plt.gca(projection=wcs)
-    ra = ax.coords['ra']
-    dec = ax.coords['dec']
-    ra.set_major_formatter('hh:mm:ss')
-    ra.set_ticks(exclude_overlapping=True)
-    dec.set_ticks(exclude_overlapping=True)
-    dec.set_major_formatter('dd:mm')
-    ra.set_ticks_position('lb')
-    ra.set_ticklabel_position('lb')
-    dec.set_ticks_position('br')
-    dec.set_ticklabel_position('br')
-    ra.set_ticklabel(fontsize=10)
-    dec.set_ticklabel(fontsize=10)
-    ax.coords.grid(color='black', ls='solid', alpha=0.5)
-
-    # finish up plot
-    ax.set_title('UT Date: {}'.format(date.strftime('%x %X')), pad=10)
-    plt.imshow(data, cmap='gray', vmin=z_interval[0], vmax=z_interval[1])
-
-    return ax
