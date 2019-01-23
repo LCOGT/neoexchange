64645f1e8032d6da63e4b2ac56e4f318eb353d7d
Joey Chatelain <jchatelain@lco.global>
2019-01-23 14:15:20 -0800

    add copyrite/license and bail on no argument

--- neoexchange/core/management/commands/get_obs.py
+++ neoexchange/core/management/commands/get_obs.py
@@ -1,5 +1,19 @@
-"""prints a list of successful observations that fit the given arguments.
-   included in list is information about group, object, time, frames, exposure time
+"""
+NEO exchange: NEO observing portal for Las Cumbres Observatory
+Copyright (C) 2016-2019 LCO
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+    Prints a list of successful observations that fit the given arguments.
+    Included in list is information about group, object, time, frames, exposure time
 """
 
 import os
@@ -11,14 +25,14 @@ from astropy.table import Table
 
 class Command(BaseCommand):
 
-    help = 'Prints a table of successful observation blocks in the DB that fit the given arguments. If given no arguments, this will print all successful blocks.'
+    help = 'Prints a table of successful observation blocks in the DB that fit the given arguments. If given no arguments, this will fail.'
 
     def add_arguments(self, parser):
         parser.add_argument('-t', '--total_time', type=float, default=0.0, help='Minimum total exposure time (m) of block')
         # parser.add_argument('-s','--start',type=float,default=0, help='Starting time (JD) of included observations')
         # parser.add_argument('-e','--end',type=float,default=Time.now().jd, help='Ending time (JD) of included observations')
-        parser.add_argument('-b', '--body', type=str, default=None, help='Find observations of specific object only')
-        parser.add_argument('-o', '--obs', type=str, default=None, help='Find specific observation based on tracking number')
+        parser.add_argument('-b', '--body', type=str, default=None, help='Find observations of specific object only. (If object name has spaces, put in quotes.)')
+        parser.add_argument('-o', '--obs', type=str, default=None, help='Find specific observation based on tracking number.')
 
     def handle(self, *args, **options):
         # initial filtering
@@ -28,7 +42,8 @@ class Command(BaseCommand):
             if options['obs']:
                 blocks = SuperBlock.objects.filter(tracking_number=options['obs'])
             else:
-                blocks = SuperBlock.objects.all()
+                msg = "No input arguments. Either use -b 'Object Name' or -o trackingnumber "
+                raise CommandError(msg)
         long_obs = np.array([])
         tab = Table(names=('Superblock ID', 'Group ID', 'Object', 'total subblocks', 'red frames', 'good frames',
                            'exposure time (m)'), dtype=('S', 'S', 'S', 'i4', 'i4', 'i4', 'f4'))
