187f0c7ad7e8bca27a08701dd90474b4a4d44f93
Joey Chatelain <jchatelain@lco.global>
2019-02-07 18:58:18 -0800

    clean update_targets.py

--- neoexchange/core/management/commands/update_targets.py
+++ neoexchange/core/management/commands/update_targets.py
@@ -27,9 +27,9 @@ logger = logging.getLogger(__name__)
 
 class Command(BaseCommand):
     help = 'This command updates objects that have been ingested in the last three months, that ' \
-    'have not been updated within 2 days and that are not from the Minor Planet Center or LCO ' \
-    'using default settings. There are three optional arguments that have choices that specify ' \
-    'what you would like to update; sources, ingest_time, update_age, and date.'
+           'have not been updated within 2 days and that are not from the Minor Planet Center or LCO ' \
+           'using default settings. There are three optional arguments that have choices that specify ' \
+           'what you would like to update; sources, ingest_time, update_age, and date.'
 
     def __init__(self):
         super(Command, self).__init__()
@@ -40,53 +40,53 @@ class Command(BaseCommand):
             type=str,
             choices=['allneos', 'nasa', 'radar', 'objects'],
             default='objects',
-            help='Updates NEOs by origin source. "allneos" updates all NEOs, "nasa" updates ' \
-            'origins assosiated with NASA, "radar" updates NEOs being followed by radar origins,' \
-            ' "objects" updates all but MPC and LCO objects, "M" is the Minor Planet Center,'\
-            ' "N" is NASA, "S" is Spacewatch, "D" is NEODSYS, "G" is Goldstone,"A" is Arecibo,' \
-            ' "R" is both Goldstone and Aricebo, "L" is LCO, "Y" is Yarkovsky.'
+            help='Updates NEOs by origin source. "allneos" updates all NEOs, "nasa" updates '
+                 'origins associated with NASA, "radar" updates NEOs being followed by radar origins, '
+                 '"objects" updates all but MPC and LCO objects, "M" is the Minor Planet Center, '
+                 '"N" is NASA, "S" is Spacewatch, "D" is NEODSYS, "G" is Goldstone,"A" is Arecibo, '
+                 '"R" is both Goldstone and Aricebo, "L" is LCO, "Y" is Yarkovsky.'
         )
         
         parser.add_argument(
             '--ingest_time',
             type=int,
             default=90,
-            help='"ingest_time" is the number of days from the "date" given in the command line.' \
-            ' The command takes in a integer value of days. The default vaule is 90 days.'
+            help='"ingest_time" is the number of days from the "date" given in the command line. '
+                 'The command takes in a integer value of days. The default value is 90 days.'
         )
 
         parser.add_argument(
             '--update_age',
             type=int,
-            default=2,           
-            help='"update_age" is the time in days from the "date" given in the command line.' \
-            ' The command takes in a interger value of hours. The default value is 2 days. '
+            default=2,
+            help='"update_age" is the time in days from the "date" given in the command line. '
+                 'The command takes in a integer value of hours. The default value is 2 days. '
         )
         
-    	parser.add_argument(
-         	'--date',
-         	type=str,
-        	default=datetime.utcnow(),
-        	help='"date" is the starting date that the updates will be centered around. ' \
-        	'The default is datetime.datetime.utcnow(). If a date is given in the command ' \
-        	'it must be a string in the format of "%y-%m-%d %H:%M:%S"; any other format ' \
-        	'will trigger a ValueError.'
-	    )
+        parser.add_argument(
+            '--date',
+            type=str,
+            default=datetime.utcnow(),
+            help='"date" is the starting date that the updates will be centered around. '
+                 'The default is datetime.datetime.utcnow(). If a date is given in the command '
+                 'it must be a string in the format of "%y-%m-%d %H:%M:%S"; any other format '
+                 'will trigger a ValueError.'
+        )
 
     def handle(self, *args, **options):
         time = options['update_age'] 
         date = options['date']
         old = options['ingest_time'] 
 
-        if options['sources']=='allneos':
+        if options['sources'] == 'allneos':
             origins = ['M', 'N', 'S', 'D', 'G', 'A', 'R', 'L', 'Y']
-        elif options['sources']=='nasa':
+        elif options['sources'] == 'nasa':
             origins = ['G', 'N']
-        elif options['sources']=='radar':
-            origins = ['G','A','R']
-        elif options['sources']=='objects':
+        elif options['sources'] == 'radar':
+            origins = ['G', 'A', 'R']
+        elif options['sources'] == 'objects':
             origins = ['N', 'S', 'D', 'G', 'A', 'R', 'Y']
         else:
-            origins = ['N', 'S', 'D', 'G', 'A', 'R', 'Y']         
-	   
+            origins = ['N', 'S', 'D', 'G', 'A', 'R', 'Y']
+
         update_neos(origins=origins, updated_time=time, ingest_limit=old, start_time=date)
