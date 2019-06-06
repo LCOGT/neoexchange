abba5f817461fa29b2f7e83069bafd2b0d320dbd
Joey Chatelain <jchatelain@lco.global>
2019-06-06 14:09:21 -0700

    Update version number/History

--- neoexchange/neox/settings.py
+++ neoexchange/neox/settings.py
@@ -7,7 +7,7 @@ from django.utils.crypto import get_random_string
 import rollbar
 
 
-VERSION = '2.8.5'
+VERSION = '2.8.6'
 
 CURRENT_PATH = os.path.dirname(os.path.realpath(__file__))
 PRODUCTION = True if CURRENT_PATH.startswith('/var/www') else False
