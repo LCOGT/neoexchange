3ccdfbfe7bfcfdd6fea97d26747dd75200f80ad4
Tim Lister <tlister@lcogt.net>
2019-07-15 17:18:42 -0700

    twiddle version number

--- neoexchange/neox/settings.py
+++ neoexchange/neox/settings.py
@@ -7,7 +7,7 @@ from django.utils.crypto import get_random_string
 import rollbar
 
 
-VERSION = '2.8.8b'
+VERSION = '2.8.9a-ELP'
 
 CURRENT_PATH = os.path.dirname(os.path.realpath(__file__))
 PRODUCTION = True if CURRENT_PATH.startswith('/var/www') else False
