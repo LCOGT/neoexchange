e26d46df648efb6b1f85e2077c29033bcbcf46f5
Joey Chatelain <jchatelain@lco.global>
2020-01-09 10:33:16 -0800

    check for links specifically when sorting rocks.

--- neoexchange/photometrics/catalog_subs.py
+++ neoexchange/photometrics/catalog_subs.py
@@ -1444,9 +1444,9 @@ def sort_rocks(fits_files):
             dest_filepath = os.path.join(object_directory, os.path.basename(fits_filepath))
             # if the file is an e91 and an e11 exists in the working directory, remove the link to the e11 and link the e91
             if 'e91' in fits_filepath:
-                if os.path.exists(dest_filepath.replace('e91.fits', 'e11.fits')):
+                if os.path.lexists(dest_filepath.replace('e91.fits', 'e11.fits')):
                     os.unlink(dest_filepath.replace('e91.fits', 'e11.fits'))
-                if not os.path.exists(dest_filepath):
+                if not os.path.lexists(dest_filepath):
                     os.symlink(fits_filepath, dest_filepath)
             # if the file is an e11 and an e91 doesn't exit in the working directory, create link to the e11
             elif 'e11' in fits_filepath and not os.path.exists(dest_filepath.replace('e11.fits', 'e91.fits')):
