133765f95d622604b6718c42a7822b03122f76aa
Tim Lister <tlister@lcogt.net>
2020-02-21 17:31:33 -0800

    Add latest of ingest vs update time to Body detail page. Fixes #398

--- neoexchange/neox/tests/test_bodies.py
+++ neoexchange/neox/tests/test_bodies.py
@@ -53,6 +53,68 @@ class BodyDetailsTest(FunctionalTest):
 
         # She notices there is a table which lists a lot more details about
         # the Body.
+        sidebar_text = self.browser.find_element_by_class_name('rightsidebar').text
+        self.assertIn("Last Update: " + self.body.update_time.strftime("%Y-%b-%d %H:%M"), sidebar_text)
+
+
+        testlines = [ 'ECCENTRICITY ' + str(self.body.eccentricity),
+                      'MEAN DISTANCE (AU) ' + str(self.body.meandist),
+                      'ABSOLUTE MAGNITUDE (H) ' + str(self.body.abs_mag),
+                      'ALBEDO (AVERAGE) 0.17',
+                      'ALBEDO (RANGE) 0.01 - 0.60',
+                      'DIAMETER IN METERS (AVERAGE) ' + str(int(round(self.body.diameter(), 0))),
+                      'DIAMETER IN METERS (RANGE) ' + str(int(round(self.body.diameter_range()[0], 0))) + ' - ' + str(int(round(self.body.diameter_range()[1], 0)))
+                    ]
+        for line in testlines:
+            self.check_for_row_in_table('id_orbelements', line)
+
+        # She notices there is another table which lists details about
+        # the follow-up of the Body.
+
+        testlines = [ 'NEOCP DIGEST2 SCORE ' + str(self.body.score),
+                      'NUMBER OF OBSERVATIONS ' + str(self.body.num_obs),
+                      'ARC LENGTH (DAYS) ' + str(round(self.body.arc_length,2)),
+                      'TIME SINCE LAST OBSERVATION (DAYS) ' + str(round(self.body.not_seen,2))
+                    ]
+
+        for line in testlines:
+            self.check_for_row_in_table('id_followup', line)
+
+    @patch('core.plots.datetime', MockDateTime)
+    def test_can_view_body_details_no_update(self):
+        MockDateTime.change_datetime(2015, 3, 19, 6, 00, 00)
+        self.body.update_time = None
+        self.body.save()
+        self.body.refresh_from_db()
+
+        # A new user comes along to the site
+        self.browser.get(self.live_server_url)
+
+        # She sees a link from the targets' name on the front page to a more
+        # detailed view.
+        link = self.browser.find_element_by_link_text('N999r0q')
+        body_url = self.live_server_url + reverse('target', kwargs={'pk': 1})
+        self.assertIn(link.get_attribute('href'), body_url)
+
+        # She clicks the link and is taken to a page with the targets' details.
+        with self.wait_for_page_load(timeout=10):
+            link.click()
+        new_url = self.browser.current_url
+        self.assertEqual(str(new_url), body_url)
+
+        # She notices the page title has the name of the site and the header
+        # mentions the current target
+        self.assertIn(self.body.current_name() + ' details | LCO NEOx', self.browser.title)
+        header_text = self.browser.find_element_by_class_name('headingleft').text
+        self.assertIn(self.body.full_name(), header_text)
+        title_text = self.browser.find_element_by_class_name('container').text
+        self.assertNotIn('Characterization Target', title_text)
+
+        # She notices there is a table which lists a lot more details about
+        # the Body.
+        sidebar_text = self.browser.find_element_by_class_name('rightsidebar').text
+        self.assertIn("Ingest Time: " + self.body.ingest.strftime("%Y-%b-%d %H:%M"), sidebar_text)
+
 
         testlines = [ 'ECCENTRICITY ' + str(self.body.eccentricity),
                       'MEAN DISTANCE (AU) ' + str(self.body.meandist),
