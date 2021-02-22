f8c0d9eff260108b8ea61015bc45ffebd4480166
Tim Lister <tlister@lcogt.net>
2021-02-22 15:25:00 -0800

    Add basic admin for CalEvents

--- neoexchange/cal/admin.py
+++ neoexchange/cal/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from cal.models import *
+
+from reversion.admin import VersionAdmin
+
+
+@admin.register(CalEvent)
+class CalEventAdmin(VersionAdmin):
+
+    list_display = ('event_type', 'start', 'end', 'resource', 'state', 'created')
+    list_filter = ('event_type', 'start', 'end', 'resource', 'state', 'created', 'modified')
+
+    ordering = ('-start',)
