124d9500f8e092610f66cc61f6b0c19d5a4a6268
Tim Lister <tlister@lcogt.net>
2021-06-23 11:42:33 -0700

    Add very basic tests for make_visibility_plot

--- neoexchange/core/tests/test_plots.py
+++ neoexchange/core/tests/test_plots.py
@@ -2,9 +2,11 @@ from unittest import skipIf
 from datetime import datetime
 
 from django.test import TestCase
+from django.http import HttpResponse
 
+from core.models import Body
 # Import module methods to test
-from core.plots import find_existing_vis_file, determine_plot_valid
+from core.plots import find_existing_vis_file, determine_plot_valid, make_visibility_plot
 
 # Disable logging during testing
 import logging
@@ -73,3 +75,60 @@ class TestDeterminePlotValid(TestCase):
         vis_file = determine_plot_valid(orig_vis_file, compare_time)
 
         self.assertEqual(orig_vis_file, vis_file)
+
+
+class TestMakeVisibilityPlot(TestCase):
+
+    def setUp(self):
+        body_params = {
+                         'provisional_name': None,
+                         'provisional_packed': 'j5432',
+                         'name': '455432',
+                         'origin': 'A',
+                         'source_type': 'N',
+                         'elements_type': 'MPC_MINOR_PLANET',
+                         'active': True,
+                         'fast_moving': True,
+                         'urgency': None,
+                         'epochofel': datetime(2019, 7, 31, 0, 0),
+                         'orbit_rms': 0.46,
+                         'orbinc': 31.23094,
+                         'longascnode': 301.42266,
+                         'argofperih': 22.30793,
+                         'eccentricity': 0.3660154,
+                         'meandist': 1.7336673,
+                         'meananom': 352.55084,
+                         'perihdist': None,
+                         'epochofperih': None,
+                         'abs_mag': 18.54,
+                         'slope': 0.15,
+                         'score': None,
+                         'discovery_date': datetime(2003, 9, 7, 3, 7, 18),
+                         'num_obs': 130,
+                         'arc_length': 6209.0,
+                         'not_seen': 3.7969329574421296,
+                         'updated': True,
+                         'ingest': datetime(2019, 7, 4, 5, 28, 39),
+                         'update_time': datetime(2019, 7, 30, 19, 7, 35)
+                        }
+        self.test_body = Body.objects.create(**body_params)
+
+        body_params['provisional_name'] = 'N999foo'
+        body_params['provisional_packed'] = None
+        body_params['name'] = None
+        self.test_neocp_body = Body.objects.create(**body_params)
+
+        self.start_time = datetime(2021,6,1)
+
+    def test_badplottype(self):
+        response = make_visibility_plot(None, self.test_body.pk, 'cucumber', self.start_time)
+
+        self.assertTrue(isinstance(response, HttpResponse))
+        self.assertEqual('image/gif', response['Content-Type'])
+        self.assertEqual(b'GIF89a', response.content[0:6])
+
+    def test_nonameobject(self):
+        response = make_visibility_plot(None, self.test_neocp_body.pk, 'radec', self.start_time)
+
+        self.assertTrue(isinstance(response, HttpResponse))
+        self.assertEqual(b'', response.content)
