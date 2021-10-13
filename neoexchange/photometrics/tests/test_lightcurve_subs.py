12ad6c5f89e7cb72b481838f965e4947bc425ffd
Tim Lister <tlister@lcogt.net>
2021-10-13 15:39:19 -0700

    Add basic reader and test for photometrypipeline tables

---
+++ neoexchange/photometrics/tests/test_lightcurve_subs.py
@@ -0,0 +1,25 @@
+import os
+import shutil
+import tempfile
+from glob import glob
+from datetime import datetime
+
+from photometrics.lightcurve_subs import *
+
+from django.test import SimpleTestCase
+
+class TestReadPhotomPipe(SimpleTestCase):
+
+    def setUp(self):
+        self.test_lc_file = os.path.abspath(os.path.join('photometrics', 'tests', 'example_photompipe.dat'))
+
+        self.debug_print = False
+        self.maxDiff = None
+
+
+    def test_read(self):
+        expected_length = 10
+
+        table = read_photompipe_file(self.test_lc_file)
+
+        self.assertEqual(expected_length, len(table))
