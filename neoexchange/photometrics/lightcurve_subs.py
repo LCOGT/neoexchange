aa1aa5e2e0af5c1295c425b133fef6879575a952
Tim Lister <tlister@lcogt.net>
2021-10-14 14:09:33 -0700

    Add routine to extract used aperture radius of log file. Use this for the column in the output lightcurve file

--- neoexchange/photometrics/lightcurve_subs.py
+++ neoexchange/photometrics/lightcurve_subs.py
@@ -14,8 +14,13 @@ GNU General Public License for more details.
 """
 
 import os
+import re
 
-from astropy.table import Table, unique
+import numpy as np
+from astropy.table import Table, unique, Column
+import logging
+
+logger = logging.getLogger(__name__)
 
 def read_photompipe_file(filepath):
     """Reads a photometry table file at <filepath> produced by the photometrypipeline and
@@ -27,16 +32,35 @@ def read_photompipe_file(filepath):
         table = Table.read(filepath, format='ascii.commented_header')
         if len(table) >= 2:
             if table['filename'][0] == table['filename'][1]:
-                print("Doubling detected")
+                logger.debug("Doubling detected")
                 table = unique(table, keys='filename')
     return table
 
-def write_dartformat_file(table, filepath):
+def extract_photompipe_aperradius(logfile):
+    """Parse through a passed photometrypipeline log file to extract the
+    aperture radius used.
+    """
+
+    aper_radius = None
+
+    if os.path.exists(logfile):
+        regex = r"aperture radius: (\d+.\d+)"
+        with open(logfile, 'r') as f:
+            for line in f.readlines():
+                matches = re.search(regex, line)
+                if matches:
+                    if len(matches.groups()) == 1:
+                        aper_radius = float(matches.group(1))
+                    else:
+                        logger.warning("Unexpected number of matches")
+    return aper_radius
+
+def write_dartformat_file(table, filepath, aprad=0.0):
     """Writes out the passed Astropy <table > in "DART lightcurve format" to the
     given <filepath>"""
 
     output_col_names = ['file', 'julian_date', 'mag', 'sig', 'ZP', 'ZP_sig', 'inst_mag', 'inst_sig', 'SExtractor_flag', 'aprad']
-    input_col_names = ['filename', 'julian_date', 'mag', 'sig', 'ZP', 'ZP_sig', 'inst_mag', 'in_sig', '[8]', 'FWHM"']
+    input_col_names = ['filename', 'julian_date', 'mag', 'sig', 'ZP', 'ZP_sig', 'inst_mag', 'in_sig', '[8]', 'aprad']
     col_starts = [0, 37, 53, 61, 68, 76, 83, 92, 99, 115]
     def_fmt = '%.4f'
     formatters = {  'julian_date' : '%15.7f', 'mag' : def_fmt, 'sig' : def_fmt,
@@ -44,9 +68,12 @@ def write_dartformat_file(table, filepath):
                     'aprad' : '%.2f'
                  }
 
-    # Replace truncated '.lda' in filename with real name
+    # Replace truncated '.lda' in filename with real name.
+    # Also add a column for aperture radius
     new_names = [x.replace('.lda','.fits') for x in table['filename']]
     table.replace_column('filename', new_names)
+    aprad_column = Column(np.full(len(table), aprad), name='aprad')
+    table.add_column(aprad_column)
     table[input_col_names].write(filepath, format='ascii.fixed_width', names=output_col_names,
         col_starts=col_starts, comment=False, delimiter='', formats=formatters)
 
