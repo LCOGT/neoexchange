53ef0701b57834b79c80457a4c1ef1a5d37b5893
Tim Lister <tlister@lco.global>
2022-09-18 22:31:08 -0700

    Add SNR and FWHM estimates

--- neoexchange/summarize_obs.py
+++ neoexchange/summarize_obs.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta
 
 import numpy as np
 
-from core.models import Block, Body, Frame
+from core.models import Block, Body, Frame, SourceMeasurement
 
 def summarize_observations(target_name='65803', start_date='2022-07-15'):
 
@@ -13,10 +13,11 @@ def summarize_observations(target_name='65803', start_date='2022-07-15'):
     if blocks.filter(site='ogg', telclass='2m0').count() > 0:
         # Set wider width for MuSCAT blocks
         filt_width = 14
-    for block in blocks.order_by('-block_start'):
-        all_frames = Frame.objects.filter(block=block, frametype=Frame.BANZAI_RED_FRAMETYPE)
+    for block in blocks.order_by('block_start'):
+        all_frames = Frame.objects.filter(block=block, frametype=Frame.NEOX_RED_FRAMETYPE)
         num_good_zp = all_frames.filter(zeropoint__gte=0).count()
         num_all_frames = all_frames.count()
+        srcs = SourceMeasurement.objects.filter(frame__block=block)
         block_length_hrs = -1
         if num_all_frames > 1:
             filters = all_frames.values_list('filter',flat=True).distinct()
@@ -32,7 +33,9 @@ def summarize_observations(target_name='65803', start_date='2022-07-15'):
                 block_length = block_end - block_start
                 block_length_hrs = block_length.total_seconds() / 3600.0
                 exp_length_hms = time.strftime('%H:%M:%S', time.gmtime(block.exp_length))
-                print(f'{block.id} {block.site} {block_start.strftime("%Y-%m-%d %H:%M")} -> {block_end.strftime("%Y-%m-%d %H:%M")} ({block_length_hrs:>5.2f} hrs) {block.superblock.get_obsdetails():14s} ({exp_length_hms}) {filter_str:{filt_width}s} {num_good_zp: 3d}/{num_all_frames: 3d}')
+                snr = np.mean(srcs.filter(frame__filter=obs_filter).values_list('snr',flat=True))
+                fwhm = np.mean(frames.values_list('fwhm', flat=True))
+                print(f'{block.id} {block.site} ({first_frame.sitecode}) {block_start.strftime("%Y-%m-%d %H:%M")} -> {block_end.strftime("%Y-%m-%d %H:%M")} ({block_length_hrs:>5.2f} hrs) {block.superblock.get_obsdetails():14s} ({exp_length_hms}) {filter_str:{filt_width}s} {num_good_zp: 3d}/{num_all_frames: 3d} SNR= {snr:.1f} FWHM= {fwhm:.1f}')
 
 def examine_multiap():
     for aprad in np.arange(2,12):
