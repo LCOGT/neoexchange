9fbacf48a7bfcccd39029e0ff2838fcc3d76460b
Cora Constantinescu <134644741+cora-const@users.noreply.github.com>
2023-06-29 22:37:39 +0000

    Added routines and tests to find blocks, frames and interpolate ephemeris

---
+++ neoexchange/core/blocksfind.py
@@ -0,0 +1,101 @@
+from core.models import Body,Block,SuperBlock,Frame
+from astropy.wcs import WCS, FITSFixedWarning, InvalidTransformError
+from datetime import datetime,timedelta
+import warnings
+from astropy.wcs import WCS, FITSFixedWarning, InvalidTransformError
+from astrometrics.ephem_subs import horizons_ephem
+from astropy.time import Time
+import numpy as np
+warnings.simplefilter('ignore', category=FITSFixedWarning)
+
+
+def find_didymos_blocks():
+    '''
+    Routine to find all of the observed didymos blocks after time of impact
+    and which are not part of the LCOEngineering or the SWOPE2022 proposals
+    Returns list of matching blocks
+    '''
+    didymos = Body.objects.get(name = '65803')
+    blocks = Block.objects.filter(body = didymos)
+    blocks = blocks.filter(num_observed__gte=1)
+    blocks = blocks.filter(block_start__gte = "2022-09-26T23:14")
+    blocks = blocks.filter(obstype = Block.OPT_IMAGING)
+    blocks = blocks.exclude(superblock__proposal__code__in=["LCOEngineering","SWOPE2022"]).order_by('block_start')
+    
+    
+    return blocks
+
+
+def blocks_summary(blocks):
+    '''
+    Prints short summary of passed <blocks>
+    '''
+    for block in blocks:
+        sblock = block.superblock
+        proposal_code = "none"
+        if sblock:
+            if sblock.proposal:
+                proposal_code=sblock.proposal.code
+        print(f"{block.block_start}->{block.block_end} {block.num_exposures}x{block.exp_length}s observed={block.num_observed} ({proposal_code})")
+        
+
+def find_frames(block):
+    '''
+    Routine to find all frames for a given block and number and type(s) 
+    of different filters
+    Returns list of frames
+    '''
+    frames = Frame.objects.filter(block = block) 
+    frames = frames.filter(frametype = Frame.BANZAI_RED_FRAMETYPE)
+    frames = frames.order_by('midpoint')
+    
+    return frames
+    
+
+def frames_summary(frames):
+    '''
+    Prints short summary of passed <frames>
+    '''
+    total_exptime = 0
+    for frame in frames:
+        delta = timedelta(seconds = frame.exptime/2)
+        print(f"{frame.block.request_number}: {frame.midpoint - delta}->{frame.midpoint + delta}  exposure time:{frame.exptime}s")
+        total_exptime = total_exptime + frame.exptime
+    
+    print(f"expected exposures: {frame.block.num_exposures}, executed exposures: {frames.count()}, total exposure time: {round(total_exptime, 2)}s")
+    
+    filter_frames = frames.order_by('filter').distinct('filter')
+    filter_names = ", ".join(filter_frames.values_list('filter',flat=True))
+    
+    print(f"number of filters: {filter_frames.count()}, filter type(s): {filter_names}")    
+    
+    
+def get_ephem(block):
+    '''
+    Creates a horizons ephemeris table for a passed <block>
+    '''
+    didymos = Body.objects.get(name = '65803')
+    frames = find_frames(block)
+    frames_summary(frames)
+    first_frame = frames[0]
+    last_frame = frames[frames.count()-1]
+    onemin = timedelta(minutes = 1)
+    delta_1 = timedelta(seconds = first_frame.exptime/2)
+    delta_2 = timedelta(seconds = last_frame.exptime/2)
+    table = horizons_ephem(didymos.current_name(), first_frame.midpoint - delta_1 - onemin, last_frame.midpoint + delta_2 + onemin, first_frame.sitecode, '1m')
+    
+    return table
+
+
+def ephem_interpolate(values, table):
+    arr1 = table['datetime_jd']
+    arr2 = table['RA']
+    arr3 = table['DEC']
+
+    result_RA = np.interp(values, arr1, arr2)
+    result_DEC = np.interp(values, arr1, arr3)
+    
+    return result_RA, result_DEC
+
+
+
---
+++ neoexchange/core/tests/tests_blocksfind.py
@@ -0,0 +1,279 @@
+import os
+from datetime import datetime, timedelta
+
+from django.test import TestCase, SimpleTestCase, override_settings
+from django.forms.models import model_to_dict
+from numpy.testing import assert_allclose
+from core.models import Proposal, SuperBlock, Block, Body, Frame
+from astropy.table import Table
+from astropy.time import Time
+
+from core.blocksfind import *
+
+# Disable logging during testing
+import logging
+logger = logging.getLogger(__name__)
+# Disable anything below CRITICAL level
+logging.disable(logging.CRITICAL)
+
+class TestFindDidymosBlocks(TestCase):
+    def setUp(self):
+
+        didymos_params = { 'name' : '65803',
+                         }
+        self.test_body = Body.objects.create(**didymos_params)
+
+        WV2997A_params = { 'name' : 'WV2997A',
+                         }
+        self.test_body2 = Body.objects.create(**WV2997A_params)
+
+        neo_proposal_params = { 'code'  : 'LCO2022A-009',
+                                'title' : 'LCOGT NEO Follow-up Network'
+                              }
+        self.neo_proposal, created = Proposal.objects.get_or_create(**neo_proposal_params)
+        
+        eng_proposal_params = { 'code'  : 'LCOEngineering',
+                                'title' : 'LCOGT Engineering'
+                              }
+        self.eng_proposal, created = Proposal.objects.get_or_create(**eng_proposal_params)
+        
+        # Create test blocks
+        sblock_params = {
+                         'body'     : self.test_body,
+                         'proposal' : self.neo_proposal,
+                         'groupid'  : 'TEMP_GROUP',
+                         'block_start' : '2022-10-20 13:00:00',
+                         'block_end'   : '2022-10-21 03:00:00',
+                         'tracking_number' : '00001',
+                         'active'   : True
+                       }
+        self.test_sblock = SuperBlock.objects.create(**sblock_params)
+        
+        block_params = { 'telclass' : '1m0',
+                         'site'     : 'CPT',
+                         'body'     : self.test_body,
+                         'superblock' : self.test_sblock,
+                         'block_start' : '2022-10-20 13:00:00',
+                         'block_end'   : '2022-10-21 03:00:00',
+                         'request_number' : '00001',
+                         'num_exposures' : 5,
+                         'exp_length' : 42.0,
+                         'num_observed' : 1,
+                         'active'   : True
+                       }
+        self.test_block = Block.objects.create(**block_params)
+
+        sblock_params = {
+                         'body'     : self.test_body,
+                         'proposal' : self.neo_proposal,
+                         'groupid'  : 'TEMP_GROUP',
+                         'block_start' : '2022-12-11 13:00:00',
+                         'block_end'   : '2022-12-12 03:00:00',
+                         'tracking_number' : '522289',
+                         'active'   : True
+                       }
+        self.test_sblock_0m4 = SuperBlock.objects.create(**sblock_params)
+        
+        block_params = { 'telclass' : '0m4',
+                         'site'     : 'CPT',
+                         'body'     : self.test_body,
+                         'superblock' : self.test_sblock_0m4,
+                         'block_start' : '2022-12-11 13:00:00',
+                         'block_end'   : '2022-12-12 03:00:00',
+                         'request_number' : '522289',
+                         'num_exposures' : 5,
+                         'exp_length' : 145.0,
+                         'num_observed' : 2,
+                         'active'   : True
+                       }
+        self.test_block_0m4 = Block.objects.create(**block_params)
+
+        sblock_params = {
+                         'body'     : self.test_body,
+                         'proposal' : self.neo_proposal,
+                         'groupid'  : 'TEMP_GROUP_spectra',
+                         'block_start' : '2022-12-11 13:00:00',
+                         'block_end'   : '2022-12-12 03:00:00',
+                         'tracking_number' : '1509481',
+                         'active'   : True
+                       }
+        self.test_sblock_spec = SuperBlock.objects.create(**sblock_params)
+        
+        block_params = { 'obstype' : Block.OPT_SPECTRA,
+                         'telclass' : '2m0',
+                         'site'     : 'coj',
+                         'body'     : self.test_body,
+                         'superblock' : self.test_sblock_spec,
+                         'block_start' : '2022-12-11 13:00:00',
+                         'block_end'   : '2022-12-12 03:00:00',
+                         'request_number' : '1509481',
+                         'num_exposures' : 1,
+                         'exp_length' : 1800.0,
+                         'num_observed' : 1,
+                         'active'   : True
+                       }
+        self.test_spec_block = Block.objects.create(**block_params)
+        
+        # create SuperBlock and Block that point to Didymos, but are from engineering proposal
+        sblock_params = {
+                         'body'     : self.test_body,
+                         'proposal' : self.eng_proposal,
+                         'groupid'  : 'TEMP_GROUP',
+                         'block_start' : '2022-10-20 13:00:00',
+                         'block_end'   : '2022-10-21 03:00:00',
+                         'tracking_number' : '00013',
+                         'active'   : True
+                       }
+        self.test_sblock_eng = SuperBlock.objects.create(**sblock_params)
+        
+        block_params = { 'telclass' : '1m0',
+                         'site'     : 'CPT',
+                         'body'     : self.test_body,
+                         'superblock' : self.test_sblock_eng,
+                         'block_start' : '2022-10-20 13:00:00',
+                         'block_end'   : '2022-10-21 03:00:00',
+                         'request_number' : '00113',
+                         'num_exposures' : 5,
+                         'exp_length' : 42.0,
+                         'num_observed' : 1,
+                         'active'   : True
+                       }
+        self.test_block_eng = Block.objects.create(**block_params)
+        
+        # create SuperBlock and Block that point to other asteroid
+        sblock_params = {
+                         'body'     : self.test_body2,
+                         'proposal' : self.neo_proposal,
+                         'groupid'  : 'TEMP_GROUP',
+                         'block_start' : '2022-10-20 13:00:00',
+                         'block_end'   : '2022-10-21 03:00:00',
+                         'tracking_number' : '00285',
+                         'active'   : True
+                       }
+        self.test_sblock_ast2 = SuperBlock.objects.create(**sblock_params)
+        
+        block_params = { 'telclass' : '1m0',
+                         'site'     : 'CPT',
+                         'body'     : self.test_body2,
+                         'superblock' : self.test_sblock,
+                         'block_start' : '2022-10-20 13:00:00',
+                         'block_end'   : '2022-10-21 03:00:00',
+                         'request_number' : '00726',
+                         'num_exposures' : 5,
+                         'exp_length' : 42.0,
+                         'num_observed' : 1,
+                         'active'   : True
+                       }
+        self.test_block_ast2 = Block.objects.create(**block_params)
+        
+    def test_didymos(self):
+        expected_num_blocks = 2
+
+        blocks = find_didymos_blocks()
+        
+        self.assertEqual(expected_num_blocks, blocks.count())
+        
+        for block in blocks:
+            self.assertTrue(hasattr(block, 'request_number'))
+            self.assertEqual(block.body, self.test_body)
+            self.assertNotEqual(block.superblock.proposal, self.eng_proposal)
+            
+class TestFindFrames(TestCase):
+    def setUp(self):
+        didymos_params = { 'name' : '65803'
+                         }
+        self.test_body = Body.objects.create(**didymos_params)
+        
+        neo_proposal_params = { 'code'  : 'LCO2022A-009',
+                                'title' : 'LCOGT NEO Follow-up Network'
+                              }
+        self.neo_proposal, created = Proposal.objects.get_or_create(**neo_proposal_params)
+        
+        # Create test blocks
+        sblock_params = {
+                         'body'     : self.test_body,
+                         'proposal' : self.neo_proposal,
+                         'groupid'  : 'TEMP_GROUP',
+                         'block_start' : '2022-10-20 13:00:00',
+                         'block_end'   : '2022-10-21 03:00:00',
+                         'tracking_number' : '00001',
+                         'active'   : True
+                       }
+        self.test_sblock = SuperBlock.objects.create(**sblock_params)
+        
+        block_params = { 'telclass' : '1m0',
+                         'site'     : 'CPT',
+                         'body'     : self.test_body,
+                         'superblock' : self.test_sblock,
+                         'block_start' : '2022-10-20 13:00:00',
+                         'block_end'   : '2022-10-21 03:00:00',
+                         'request_number' : '00001',
+                         'num_exposures' : 5,
+                         'exp_length' : 42.0,
+                         'num_observed' : 1,
+                         'active'   : True
+                       }
+        self.test_block = Block.objects.create(**block_params)
+
+        frame_params = { 'sitecode' : 'K92',
+                          'filter' : 'w',
+                          'block' : self.test_block,
+                          'midpoint' : datetime(2022,10,20,15,0,0),
+                          'frametype' : Frame.BANZAI_RED_FRAMETYPE
+                        }
+        self.test_frames = []
+        
+        for frame_num in range(block_params['num_exposures']):
+            frame_params['midpoint'] += timedelta(minutes = frame_num * 10)
+            test_frame = Frame.objects.create(**frame_params)
+            self.test_frames.append(test_frame)
+        
+        #create ldac catalog versions of the same frame
+        frame_params['midpoint'] =  datetime(2022,10,20,15,0,0)
+        frame_params['frametype'] = Frame.BANZAI_LDAC_CATALOG   
+        for frame_num in range(block_params['num_exposures']):
+            frame_params['midpoint'] += timedelta(minutes = frame_num * 10)
+            test_frame = Frame.objects.create(**frame_params)
+        
+    def test_expected(self):
+        self.assertEqual(1, Block.objects.all().count())
+        self.assertEqual(10, Frame.objects.all().count())
+        
+    def test_didymos_block(self):
+        expected_num_frames = 5
+
+        frames = find_frames(self.test_block)
+        
+        self.assertEqual(expected_num_frames, frames.count())
+        self.assertEqual(self.test_frames[0].midpoint, frames[0].midpoint)
+        self.assertEqual(self.test_frames[-1].midpoint, frames[frames.count()-1].midpoint)
+
+
+class TestEphemInterpolate(SimpleTestCase):
+    def setUp(self):
+        self.test_ephem = Table.read(os.path.join('core', 'tests', 'test_ephem.fits'))
+        
+    def test_jd_value(self):
+        expected_RA = 118.80145
+        expected_DEC = 7.948245
+        
+        t = Time(datetime(2022, 11, 4, 9, 35, 30))
+        
+        result_RA, result_DEC = ephem_interpolate(t.jd, self.test_ephem)
+        
+        assert_allclose(expected_RA, result_RA, rtol=1e-8)
+        assert_allclose(expected_DEC, result_DEC, rtol=1e-8)
+        
+    def test_jd_values(self):
+        expected_RA = [118.80145, 118.80154]
+        expected_DEC = [7.948245, 7.9483575]
+        
+        t = Time(datetime(2022, 11, 4, 9, 35, 30))
+        t_2 = Time(datetime(2022, 11, 4, 9, 35, 45))
+
+        result_RA, result_DEC = ephem_interpolate([t.jd, t_2.jd], self.test_ephem)
+        
+        assert_allclose(expected_RA, result_RA, rtol=1e-8)
+        assert_allclose(expected_DEC, result_DEC, rtol=1e-8)
+        
+        
