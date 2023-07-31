ad75ecea03abe831c6de58c1f17e63049ad7cb9c
Cora Constantinescu <134644741+cora-const@users.noreply.github.com>
2023-07-31 16:19:11 -0700

    Added a command line option that will skip the copying, stacking, and chiseling processes and will instead check for missing data/directories.

--- neoexchange/core/management/commands/make_tail_stacks.py
+++ neoexchange/core/management/commands/make_tail_stacks.py
@@ -21,6 +21,7 @@ class Command(BaseCommand):
         parser.add_argument('days_inc', type=float, help='Increment days')
         parser.add_argument('--sci_dir', default=settings.DATA_ROOT, help='Directory where input files are stored')
         parser.add_argument('--dest_dir', default=os.path.join(settings.DATA_ROOT, 'Stacks'), help='Directory where output files will be written')
+        parser.add_argument('--validate_only', type=bool, default=False, help='If set to True, the command will skip the copying, stacking, and chiseling processes. It will instead check for missing data/directories')
 
     def handle(self, *args, **options):
         start_date = parse(options['start_date'])
@@ -28,6 +29,7 @@ class Command(BaseCommand):
         date_increment = timedelta(days=options['days_inc']) 
         sci_dir = options['sci_dir']
         dest_dir = options['dest_dir']
+        validate_only = options['validate_only']
 
         #lists for astropy data table
         blocks_start = []
@@ -76,6 +78,7 @@ class Command(BaseCommand):
             #set up working directory for Block and make a copy of all frames
             dayobs = block.get_blockdayobs
             blockuid = block.get_blockuid
+            self.stdout.write(f'DAYOBS: {dayobs}')
             self.stdout.write(f'BLKUID: {blockuid}')
 
             #chooses correct input path
@@ -92,65 +95,70 @@ class Command(BaseCommand):
                 else:
                     file_path_2 = ''
                 if os.path.exists(file_path_1) is False and os.path.exists(file_path_2) is False:
+                    if validate_only:
+                        self.stdout.write(f'File Not Found: {file_path_1}')
                     frames = frames.exclude(filename = frame.filename)
                     continue
                 if os.path.exists(file_path_1):
                     input_data_path = input_data_path_1
                 else:
                     input_data_path = input_data_path_2
-                shutil.copy(os.path.join(input_data_path,frame.filename), output_path)
+                if validate_only is False:
+                    shutil.copy(os.path.join(input_data_path,frame.filename), output_path)
             sci_dir_path = output_path
             dest_dir_path = os.path.join(dest_dir, dayobs)
 
             #make a record of stack midpoint, stack total exposure time, and moon fraction (need to get from fits header)
-            midpoint = block.block_start + (block.block_end - block.block_start)/2
-            total_exptime = 0
-            moon_fractions = []
-            moon_dist = []
-            for frame in frames:
-                total_exptime = total_exptime + frame.exptime
-                hdulist = fits.open(os.path.join(sci_dir_path, frame.filename))
-                header = hdulist['SCI'].header
-                moon_fractions.append(header['MOONFRAC'])
-                moon_dist.append(header['MOONDIST'])
-            avg_moon_frac = round(sum(moon_fractions)/len(moon_fractions), 4)
-            avg_moon_dist = round(sum(moon_dist)/len(moon_dist), 4)
-            emp = block.body.compute_position(midpoint)
-            V_mag = emp[2]
+            if validate_only is False:
+                midpoint = block.block_start + (block.block_end - block.block_start)/2
+                total_exptime = 0
+                moon_fractions = []
+                moon_dist = []
+                for frame in frames:
+                    total_exptime = total_exptime + frame.exptime
+                    hdulist = fits.open(os.path.join(sci_dir_path, frame.filename))
+                    header = hdulist['SCI'].header
+                    moon_fractions.append(header['MOONFRAC'])
+                    moon_dist.append(header['MOONDIST'])
+                avg_moon_frac = round(sum(moon_fractions)/len(moon_fractions), 4)
+                avg_moon_dist = round(sum(moon_dist)/len(moon_dist), 4)
+                emp = block.body.compute_position(midpoint)
+                V_mag = emp[2]
 
-            self.stdout.write(f'Block Start Time: {block.block_start.strftime("%Y-%m-%d %H:%M")}, Midpoint: {midpoint}, Total Exposure Time: {total_exptime}, Average Moon Fraction: {avg_moon_frac}')
+                self.stdout.write(f'Block Start Time: {block.block_start.strftime("%Y-%m-%d %H:%M")}, Midpoint: {midpoint}, Total Exposure Time: {total_exptime}, Average Moon Fraction: {avg_moon_frac}')
 
-            #add values for astropy data table
-            blocks_start.append(block.block_start)
-            blocks_mid.append(midpoint)
-            blocks_end.append(block.block_end)
-            exposure_time.append(total_exptime)
-            moon_frac.append(avg_moon_frac)
-            moon_distance.append(avg_moon_dist)
-            V_mags.append(V_mag)
-            block_uid.append(blockuid[0])
-            input_data_paths.append(input_data_path)
-            output_data_paths.append(output_path)
+                #add values for astropy data table
+                blocks_start.append(block.block_start)
+                blocks_mid.append(midpoint)
+                blocks_end.append(block.block_end)
+                exposure_time.append(total_exptime)
+                moon_frac.append(avg_moon_frac)
+                moon_distance.append(avg_moon_dist)
+                V_mags.append(V_mag)
+                block_uid.append(blockuid[0])
+                input_data_paths.append(input_data_path)
+                output_data_paths.append(output_path)
 
-            #call run_astwarp_alignment(), and run_noisechisel()
-            chiseled_filenames, combined_filenames, status = run_astwarp_alignment_noisechisel(block, sci_dir_path, dest_dir_path)
-            #call convert_fits_to_pdf()
-            pdf_filenames_chiseled = []
-            pdf_filenames_combined = []
-            for chiseled_filename in chiseled_filenames:
-                pdf_filename_chiseled, status = convert_fits_to_pdf(chiseled_filename, dest_dir_path)
-                pdf_filenames_chiseled.append(pdf_filename_chiseled)
-            for combined_filename in combined_filenames:
-                pdf_filename_combined, status = convert_fits_to_pdf(combined_filename, dest_dir_path)
-                pdf_filenames_combined.append(pdf_filename_combined)
-            self.stdout.write(f'Chiseled filename(s): {pdf_filenames_chiseled}')
-            self.stdout.write(f'Combined filename(s): {pdf_filenames_combined}')
-            self.stdout.write('')
+                #call run_astwarp_alignment(), and run_noisechisel()
+                chiseled_filenames, combined_filenames, status = run_astwarp_alignment_noisechisel(block, sci_dir_path, dest_dir_path)
+                #call convert_fits_to_pdf()
+                pdf_filenames_chiseled = []
+                pdf_filenames_combined = []
+                for chiseled_filename in chiseled_filenames:
+                    pdf_filename_chiseled, status = convert_fits_to_pdf(chiseled_filename, dest_dir_path)
+                    pdf_filenames_chiseled.append(pdf_filename_chiseled)
+                for combined_filename in combined_filenames:
+                    pdf_filename_combined, status = convert_fits_to_pdf(combined_filename, dest_dir_path)
+                    pdf_filenames_combined.append(pdf_filename_combined)
+                self.stdout.write(f'Chiseled filename(s): {pdf_filenames_chiseled}')
+                self.stdout.write(f'Combined filename(s): {pdf_filenames_combined}')
+                self.stdout.write('')
 
             current_time += date_increment
 
-        #make astropy table and convert to csv file
-        table = QTable([blocks_start, blocks_mid, blocks_end, exposure_time, moon_frac, moon_distance, V_mags, block_uid, input_data_paths, output_data_paths],
-                names=('Block Start', 'Block Midpoint', 'Block End', 'Exposure Time', 'Moon Fraction', 'Moon Distance', 'V Magnitude', 'Block UID', 'Input Path', 'Output Path'))
-        csv_path = os.path.join(dest_dir, 'didymos_tail_data.csv')
-        ascii.write(table, csv_path, format='ecsv', delimiter = ',')
+        if validate_only is False:
+            #make astropy table and convert to csv file
+            table = QTable([blocks_start, blocks_mid, blocks_end, exposure_time, moon_frac, moon_distance, V_mags, block_uid, input_data_paths, output_data_paths],
+                    names=('Block Start', 'Block Midpoint', 'Block End', 'Exposure Time', 'Moon Fraction', 'Moon Distance', 'V Magnitude', 'Block UID', 'Input Path', 'Output Path'))
+            csv_path = os.path.join(dest_dir, 'didymos_tail_data.csv')
+            ascii.write(table, csv_path, format='ecsv', delimiter = ',')
