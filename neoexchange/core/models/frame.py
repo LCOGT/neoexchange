1c37a1f73f771e8e62184a5e850ca7155dabb4fe
Tim Lister <tlister@lcogt.net>
2023-09-18 23:47:21 +0000

    Add MRO frame types and migration

--- neoexchange/core/models/frame.py
+++ neoexchange/core/models/frame.py
@@ -78,7 +78,10 @@ class Frame(models.Model):
     BANZAI_LDAC_CATALOG = 6
     ORACDR_QL_FRAMETYPE = 10
     BANZAI_QL_FRAMETYPE = 11
-    SWOPE_RED_FRAMETYPE = 70
+    MRO_RAW_FRAMETYPE = 60
+    MRO_RED_FRAMETYPE = 61
+    SWOPE_RAW_FRAMETYPE = 70
+    SWOPE_RED_FRAMETYPE = 71
     REFERENCE_FRAMETYPE = 80
     ORACDR_RED_FRAMETYPE = 90
     BANZAI_RED_FRAMETYPE = 91
@@ -94,6 +97,9 @@ class Frame(models.Model):
                         (BANZAI_LDAC_CATALOG,  'BANZAI LDAC catalog'),
                         (ORACDR_QL_FRAMETYPE,  'ORACDR QL frame'),
                         (BANZAI_QL_FRAMETYPE,  'BANZAI QL frame'),
+                        (MRO_RAW_FRAMETYPE,  'MRO raw frame'),
+                        (MRO_RED_FRAMETYPE,  'MRO reduced frame'),
+                        (SWOPE_RAW_FRAMETYPE,  'Swope raw frame'),
                         (SWOPE_RED_FRAMETYPE,  'Swope reduced frame'),
                         (REFERENCE_FRAMETYPE,  'Reference frame'),
                         (ORACDR_RED_FRAMETYPE, 'ORACDR reduced frame'),
